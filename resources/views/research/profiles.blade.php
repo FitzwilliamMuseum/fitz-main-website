9765353b57a1f0a2c4a7a10d860cc4c4795a45b1
Daniel Pett <dejp3@cam.ac.uk>
2020-04-14 22:20:55 +0100

    Update the title

--- resources/views/research/profiles.blade.php
+++ resources/views/research/profiles.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts/layout')
 @section('title','Researcher profiles')
 @section('hero_image','https://fitz-cms-images.s3.eu-west-2.amazonaws.com/baroque.jpg')
+@section('hero_image_title', 'The baroque feasting table by Ivan Day in Feast and Fast')
 @section('content')
 <div class="row">
     @foreach($profiles['data'] as $project)
