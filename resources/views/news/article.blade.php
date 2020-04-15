a7c29a839cfad62cc9277e01372157db1012f721
Daniel Pett <dejp3@cam.ac.uk>
2020-04-16 00:58:18 +0100

    Add missing >

--- resources/views/news/article.blade.php
+++ resources/views/news/article.blade.php
@@ -10,7 +10,7 @@
   @section('content')
       <h2>{{ $project['article_title'] }}</h2>
       <div class="col-12 shadow-sm p-3 mx-auto mb-3 rounded">
-        <h3>{{  Carbon\Carbon::parse($project['publication_date'])->format('l dS F Y') }}</h3
+        <h3>{{  Carbon\Carbon::parse($project['publication_date'])->format('l dS F Y') }}</h3>
         @markdown($project['article_body'])
       </div>
   @endsection
