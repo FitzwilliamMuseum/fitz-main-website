130da23fa277b872a1446c3ef3893dfcd3a75768
Daniel Pett <dejp3@cam.ac.uk>
2020-04-15 19:38:58 +0100

    Change hashtag

--- resources/views/includes/announcement.blade.php
+++ resources/views/includes/announcement.blade.php
@@ -3,7 +3,7 @@
     <p class="text-center">
       <i class="fa fa-exclamation-circle fa-inverse"></i>
       The Fitzwilliam Museum is currently closed due to Covid-19.
-      Join us online as we bring you #virtualFitz.
+      Join us online as we bring you #FitzVirtual.
     </p>
   </div>
 </div>
