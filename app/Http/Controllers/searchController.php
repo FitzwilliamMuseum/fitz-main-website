ae0f56e5466a183c6a20c4d3d0cdb33dfb8f8116
Daniel Pett <dejp3@cam.ac.uk>
2020-04-24 16:59:48 +0100

    Change operator

--- app/Http/Controllers/searchController.php
+++ app/Http/Controllers/searchController.php
@@ -8,6 +8,7 @@ use Solarium\Exception;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Cache;
 use Mews\Purifier;
+use App\Directus;
 
 class searchController extends Controller
 {
@@ -54,7 +55,7 @@ class searchController extends Controller
           $this->client = new Client($configSolr);
           $query = $this->client->createSelect();
           $query->setQuery($queryString);
-          $query->setQueryDefaultOperator('AND');
+          $query->setQueryDefaultOperator('OR');
           $query->setStart($from);
           $query->setRows($perPage);
           $data = $this->client->select($query);
@@ -83,5 +84,88 @@ class searchController extends Controller
       } catch (\Exception $e) {
           return response()->json('ERROR', 500);
       }
+  }
+
+  public function staff()
+  {
+    $api = $this->getApi();
+    $api->setEndpoint('staff_profiles');
+    $api->setArguments(
+      $args = array(
+          'limit' => '500',
+          'fields' => 'id,display_name,biography, slug'
+      )
+    );
+    $url = '/research/staff-profiles/';
+    $profiles = $api->getData();
+    $configSolr = \Config::get('solarium');
+    $this->client = new Client($configSolr);
+    $update = $this->client->createUpdate();
+    $documents = array();
+    foreach($profiles['data'] as $profile)
+    {
+      $doc = $update->createDocument();
+      $doc->id = $profile['id'] . '-staff';
+      $doc->title = $profile['display_name'];
+      $doc->description = strip_tags($profile['biography']);
+      $doc->slug = $profile['slug'];
+      $doc->url = $url;
+      $doc->contentType = 'staffProfile';
+      $documents[] = $doc;
+
+    }
+
+    // add the documents and a commit command to the update query
+    $update->addDocuments($documents);
+    $update->addCommit();
+    // this executes the query and returns the result
+    $result = $this->client->update($update);
+  }
+
+  public function news()
+  {
+    $api = $this->getApi();
+    $api->setEndpoint('news_articles');
+    $api->setArguments(
+      $args = array(
+          'limit' => '500',
+          'fields' => 'id,article_title,article_body,slug'
+      )
+    );
+    $url = '/news/article/';
+    $profiles = $api->getData();
+    $configSolr = \Config::get('solarium');
+    $this->client = new Client($configSolr);
+    $update = $this->client->createUpdate();
+    $documents = array();
+    foreach($profiles['data'] as $profile)
+    {
+      $doc = $update->createDocument();
+      $doc->id = $profile['id'] . '-news';
+      $doc->title = $profile['article_title'];
+      $doc->description = strip_tags($profile['article_body']);
+      $doc->slug = $profile['slug'];
+      $doc->url = $url;
+      $doc->contentType = 'news';
+      $documents[] = $doc;
+    }
+    // add the documents and a commit command to the update query
+    $update->addDocuments($documents);
+    $update->addCommit();
+    // this executes the query and returns the result
+    $result = $this->client->update($update);
+  }
+
+
+
+  function change_key( $array, $old_key, $new_key ) {
+
+    if( ! array_key_exists( $old_key, $array ) )
+        return $array;
+
+    $keys = array_keys( $array );
+    $keys[ array_search( $old_key, $keys ) ] = $new_key;
+
+    return array_combine( $keys, $array );
   }
 }
