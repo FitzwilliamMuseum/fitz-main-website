a7abd019534b012069f1d9348a62781c9ffe5aa7
Daniel Pett <dejp3@cam.ac.uk>
2020-04-28 12:06:54 +0100

    Change text

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -55,7 +55,7 @@ class AppServiceProvider extends ServiceProvider
             case "department":
               $clean = 'Department information';
             break;
-            case "directors":
+            case "director":
               $clean = 'About our directors';
             break;
             default:
