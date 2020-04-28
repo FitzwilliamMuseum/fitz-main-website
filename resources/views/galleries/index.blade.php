cc4665e48b9c3738feab7a186a18734d98203c73
Daniel Pett <dejp3@cam.ac.uk>
2020-04-28 22:59:38 +0100

    Remove content

--- resources/views/galleries/index.blade.php
+++ resources/views/galleries/index.blade.php
@@ -4,11 +4,7 @@
 @section('hero_image', $page['hero_image']['data']['full_url'])
 @section('hero_image_title', $page['hero_image_alt_text'])
 
-@section('content')
-  <div class="col-12 shadow-sm p-3 mx-auto mb-3 rounded ">
-  @markdown($page['body'])
-  </div>
-@endsection
+
 @endforeach
 
 @section('themes')
