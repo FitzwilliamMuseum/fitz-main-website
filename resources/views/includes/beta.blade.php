7c2dab118b43315e023bc79bf44c34d37d587a65
Daniel Pett <dejp3@cam.ac.uk>
2020-04-30 10:05:18 +0100

    Amend wording

--- resources/views/includes/beta.blade.php
+++ resources/views/includes/beta.blade.php
@@ -1,8 +1,8 @@
 <div class="container-fluid beta ">
   <div class="col-md-12">
     <p class="text-center share">
-      This site is a Beta release of our new website. We're trying to co-design
-      our Virtual Museum presence with you.
+      This is a Beta release of our new website. We're trying to co-design
+      our Virtual Museum presence with you. 
     </p>
   </div>
 </div>
