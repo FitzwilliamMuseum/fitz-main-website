472d91a3d394bb79d309e77e2a5183f6106a0162
Daniel Pett <dejp3@cam.ac.uk>
2020-05-05 07:48:19 +0100

    Change layout for tweets and instagram

--- resources/views/includes/social/insta.blade.php
+++ resources/views/includes/social/insta.blade.php
@@ -3,16 +3,15 @@
 <div class="col-md-4 mb-3">
   <div class="card card-body h-100">
     <a href="{{ $media->getLink() }}"><img class="img-fluid" src="{{ $media->getImageThumbnailUrl() }}"/></a>
-  <div class="container h-100">
+    <div class="container h-100">
 
-    <div class="contents-label mb-3">
-      <p class="card-text">{{ substr(strip_tags(htmlspecialchars_decode($media->getCaption())),0,200) }}...</p>
-      <p>Comments: {{ $media->getCommentsCount() }} Favourited: {{$media->getLikesCount()}} </p>
+      <div class="contents-label mb-3">
+        <p class="card-text">{{ substr(strip_tags(htmlspecialchars_decode($media->getCaption())),0,200) }}...</p>
+        <p>Interactions: {{ $media->getCommentsCount() }} Favourited: {{$media->getLikesCount()}} </p>
+      </div>
     </div>
+    <a href="" class="btn btn-dark">Read more</a>
   </div>
-  <a href="" class="btn btn-dark">Read more</a>
-</div>
-
 </div>
 @endforeach
 </div>
