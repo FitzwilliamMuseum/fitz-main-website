bcee31719369956ce3cfda2ec07715e8f2c3a1ca
Daniel Pett <dejp3@cam.ac.uk>
2020-05-05 14:10:29 +0100

    Hide text

--- resources/views/includes/social/insta.blade.php
+++ resources/views/includes/social/insta.blade.php
@@ -2,15 +2,16 @@
 @foreach($insta as $media)
 <div class="col-md-4 mb-3">
   <div class="card card-body h-100">
-    <a href="{{ $media->getLink() }}"><img class="img-fluid" src="{{ $media->getImageThumbnailUrl() }}"/></a>
+    <a href="{{ $media->getLink() }}"><img class="img-fluid"
+      src="{{ $media->getImageThumbnailUrl() }}" alt="{{ substr(strip_tags(htmlspecialchars_decode($media->getCaption())),0,100) }}..."/></a>
     <div class="container h-100">
 
-      <div class="contents-label mb-3">
+      <!-- <div class="contents-label mb-3">
         <p class="card-text">{{ substr(strip_tags(htmlspecialchars_decode($media->getCaption())),0,200) }}...</p>
-      </div>
+      </div> -->
     </div>
-    <p>Interactions: {{ $media->getCommentsCount() }} Favourited: {{$media->getLikesCount()}} </p>
-    <a href="{{ $media->getLink() }}" class="btn btn-dark">Read more</a>
+    <!-- <p>Interactions: {{ $media->getCommentsCount() }} Favourited: {{$media->getLikesCount()}} </p> -->
+    <!-- <a href="{{ $media->getLink() }}" class="btn btn-dark">Read more</a> -->
   </div>
 </div>
 @endforeach
