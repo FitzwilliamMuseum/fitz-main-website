82d5eb35e587260d877d29ab9930de90bc168c62
Daniel Pett <dejp3@cam.ac.uk>
2020-05-07 08:47:56 +0100

    Push GA code

--- resources/views/includes/scripts/javascript.blade.php
+++ resources/views/includes/scripts/javascript.blade.php
@@ -50,11 +50,11 @@ $('.popover-dismiss').popover({
 </script>
 
 
-<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('app.google.analytics') }}"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ env('APP_GOOGLE_ANALYTICS') }}"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', {{ config('app.google.analytics') }});
+  gtag('config', {{ env('APP_GOOGLE_ANALYTICS') }});
 </script>
