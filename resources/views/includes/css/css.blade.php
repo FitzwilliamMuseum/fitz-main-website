5798aecd60154521355fcab0d1c39a21971f6faf
Daniel Pett <dejp3@cam.ac.uk>
2020-05-11 00:06:33 +0100

    Css speed up

--- resources/views/includes/css/css.blade.php
+++ resources/views/includes/css/css.blade.php
@@ -2,7 +2,7 @@
 <!-- Add Material font (Roboto) and Material icon as needed -->
 <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
 
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
 integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <link href="{{ URL::to('/css/material.min.css') }}" rel="stylesheet" media="screen">
