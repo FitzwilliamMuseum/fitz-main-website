393977f469931ded53055a66f10535efa7e29965
Daniel Pett <dejp3@cam.ac.uk>
2020-05-10 17:12:52 +0100

    Add label

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -15,7 +15,7 @@
   </div>
 </div>
 <a tabindex="0" class="further-info fa-stack fa-lg float-right mr-4 mb-4" role="button" data-toggle="popover"
-data-placement="left" data-content="@yield('hero_image_title')">
+data-placement="left" data-content="@yield('hero_image_title')" aria-label="Image caption reveal">
   <i class="fa fa-circle fa-stack-2x"></i>
   <i class="fa fa-info fa-stack-1x fa-inverse"></i>
 </a>
