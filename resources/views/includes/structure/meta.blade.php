c93edbd29aebb4eeb287f28dcabd37aec6774ace
Daniel Pett <dejp3@cam.ac.uk>
2020-05-12 00:50:35 +0100

    Add prefetch

--- resources/views/includes/structure/meta.blade.php
+++ resources/views/includes/structure/meta.blade.php
@@ -26,6 +26,13 @@
     <meta name="twitter:image" content="{{ URL::to('/images/') }}" />
     <meta name="twitter:creator" content="@yield('twitter_id', '@fitzmuseum_uk')" />
 
+
+    <link rel="dns-prefetch" href="//fonts.googleapis.com">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="dns-prefetch" href="//www.google-analytics.com">
+    <link rel="dns-prefetch" href="//www.googletagmanager.com">
+    <link rel="dns-prefetch" href="//s3.amazonaws.com">
+    <link rel="dns-prefetch" href="//use.fontawesome.com">
     <!--
   d888888P dP                  88888888b oo   dP                       oo dP dP oo                        8888ba.88ba
    88    88                    88             88                          88 88                           88  `8b  `8b
