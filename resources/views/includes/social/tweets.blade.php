54fa4c1382552e8691ff171d7924f47cc9e62e57
Daniel Pett <dejp3@cam.ac.uk>
2020-05-15 00:12:13 +0100

    Amend html for tweets

--- resources/views/includes/social/tweets.blade.php
+++ resources/views/includes/social/tweets.blade.php
@@ -1,16 +1,16 @@
 <div class="row">
   @foreach($tweets as $tweet)
   <div class="col-md-4 mb-3 h-100">
-    @if(isset($tweet->extended_entities))
-    @foreach($tweet->extended_entities as $entity)
-      <img class="img-fluid" src="{{ $entity[0]->media_url_https }}:small"
-      width="680" height="672" loading="lazy" alt="An image from Twitter"/>
-    @endforeach
-    @else
-      <img class="img-fluid" src="https://pbs.twimg.com/profile_images/1057934206368710656/NE1KayiE.jpg"/>
-    @endif
-    <div class="card card-body h-100">
 
+    <div class="card card-body h-100">
+      @if(isset($tweet->extended_entities))
+      @foreach($tweet->extended_entities as $entity)
+        <img class="img-fluid" src="{{ $entity[0]->media_url_https }}:small"
+        width="680" height="672" loading="lazy" alt="An image from Twitter"/>
+      @endforeach
+      @else
+        <img class="img-fluid" src="https://pbs.twimg.com/profile_images/1057934206368710656/NE1KayiE.jpg"/>
+      @endif
       <div class="container h-100">
         <div class="contents-label mb-3">
           <p>{!! Twitter::linkify($tweet->full_text) !!}</p>
