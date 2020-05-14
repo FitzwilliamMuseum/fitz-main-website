72690c6d37366809f770bfbf3c3772565c5f32e5
Daniel Pett <dejp3@cam.ac.uk>
2020-05-14 15:19:12 +0100

    Add correct title

--- resources/views/includes/social/tweets.blade.php
+++ resources/views/includes/social/tweets.blade.php
@@ -1,15 +1,22 @@
 <div class="row">
   @foreach($tweets as $tweet)
-  <div class="col-md-4 mb-3">
+  <div class="col-md-4 mb-3 h-100">
+    @if(isset($tweet->extended_entities))
+    @foreach($tweet->extended_entities as $entity)
+      <img class="img-fluid" src="{{ $entity[0]->media_url_https }}:small"
+      width="680" height="672" loading="lazy" alt="An image from Twitter"/>
+    @endforeach
+    @else
+      <img class="img-fluid" src="https://pbs.twimg.com/profile_images/1057934206368710656/NE1KayiE.jpg"/>
+    @endif
     <div class="card card-body h-100">
-      <img class="img-fluid" src="{{ $tweet->user->profile_banner_url}}"/>
+
       <div class="container h-100">
         <div class="contents-label mb-3">
-          <blockquote class="mt-3"><p>{!! Twitter::linkify($tweet->text) !!}</p></blockquote>
+          <p>{!! Twitter::linkify($tweet->full_text) !!}</p>
         </div>
       </div>
       <p>Retweets: {{ $tweet->retweet_count }} Favourited: {{ $tweet->favorite_count }}</p>
-
       <a href="{{ Twitter::linkTweet($tweet) }}" class="btn btn-dark">Read more</a>
     </div>
   </div>
