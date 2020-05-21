903798ab381677fd45ddeaa898f2feb3c7d760ec
Daniel Pett <dejp3@cam.ac.uk>
2020-05-21 01:45:34 +0100

    Add session

--- resources/views/learning/session.blade.php
+++ resources/views/learning/session.blade.php
@@ -6,8 +6,13 @@
   @section('description', $page['meta_description'])
   @section('keywords', $page['meta_keywords'])
     @section('content')
+    <h3>Session Description</h3>
     <div class="col-12 shadow-sm p-3 mx-auto mb-3 rounded ">
-    @markdown($page['body'])
+    @markdown($page['description'])
+    </div>
+    <h3>Format of the session</h3>
+    <div class="col-12 shadow-sm p-3 mx-auto mb-3 rounded ">
+    @markdown($page['format_session'])
     </div>
     @endsection
 @endforeach
