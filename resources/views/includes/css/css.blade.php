4b6d0409e6fa7f1999f436a63027dfe00a15c93f
Daniel Pett <dejp3@cam.ac.uk>
2020-06-03 22:32:33 +0100

    Amend center

--- resources/views/includes/css/css.blade.php
+++ resources/views/includes/css/css.blade.php
@@ -16,7 +16,7 @@ integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeV
   background-image: url(@yield('hero_image'));
   background-repeat: no-repeat;
   /* background-size: cover; */
-  background-position: center;
+  background-position: center center;
 }
 
 @hasSection('parallax_two')
