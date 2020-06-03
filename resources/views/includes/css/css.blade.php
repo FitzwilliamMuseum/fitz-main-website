3bdb72f5d1bf241d3320555ed0dddfcb941b4ec8
Daniel Pett <dejp3@cam.ac.uk>
2020-06-03 22:37:43 +0100

    Try scroll

--- resources/views/includes/css/css.blade.php
+++ resources/views/includes/css/css.blade.php
@@ -16,7 +16,8 @@ integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeV
   background-image: url(@yield('hero_image'));
   background-repeat: no-repeat;
   background-size: cover;
-  background-position: center center;
+  background-position: center;
+  background-attachment: scroll;
 }
 
 @hasSection('parallax_two')
