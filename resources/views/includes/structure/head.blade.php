317645ca024ae7cb2cd5aee7b327d4b1f0b4d6c8
Daniel Pett <dejp3@cam.ac.uk>
2020-06-06 16:32:08 +0100

    Move more info button

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -1,10 +1,16 @@
 <div class="container-fluid head parallax position-relative">
     <div class="absolute-bottom w-100">
-      <h1 class="shout ">@yield('title')</h1>
+    <div class="mr-3">  
     <a tabindex="0" class="further-info fa-stack fa-lg float-right mr-4 mb-4" role="button" data-toggle="popover"
     data-placement="left" data-content="@yield('hero_image_title')">
       <i class="fa fa-circle fa-stack-2x"></i>
       <i class="fa fa-info fa-stack-1x fa-inverse"></i>
     </a>
+  </div>
+  </div>
+</div>
+<div class="container-fluid bg-black">
+  <div class="ml-3 pt-3 pb-2">
+    <h1 class="shout">@yield('title')</h1>
   </div>
 </div>
