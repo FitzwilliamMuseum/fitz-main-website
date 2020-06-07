a12f2d6156be32be3b9279d8696b03015e0d20cb
Daniel Pett <dejp3@cam.ac.uk>
2020-06-07 19:27:35 +0100

    Add padding

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid head parallax position-relative">
     <div class="absolute-bottom w-100">
-    <div class="mr-3">  
+    <div class="mr-3">
     <a tabindex="0" class="further-info fa-stack fa-lg float-right mr-4 mb-4" role="button" data-toggle="popover"
     data-placement="left" data-content="@yield('hero_image_title')">
       <i class="fa fa-circle fa-stack-2x"></i>
@@ -10,7 +10,7 @@
   </div>
 </div>
 <div class="container-fluid bg-black">
-  <div class="ml-3 pt-3 pb-2">
+  <div class="ml-3 pt-3 pb-2 mt-3">
     <h1 class="shout">@yield('title')</h1>
   </div>
 </div>
