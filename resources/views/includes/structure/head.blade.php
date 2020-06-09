fa0355bb39aaf0b796d9d19c815ab70143093397
Daniel Pett <dejp3@cam.ac.uk>
2020-06-09 23:08:36 +0100

    Amend css and style for large image

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -7,8 +7,9 @@
       <i class="fa fa-info fa-stack-1x fa-inverse"></i></a>
     </div>
     <div class="mr-3">
-      <a tabindex="0" class="further-info fa-stack fa-lg float-right mr-4 mb-4" role="button" data-toggle="lightbox"
-      data-remote="@yield('hero_image')" data-content="@yield('hero_image_title')">
+      <a tabindex="0" class="further-info fa-stack fa-lg float-right mr-4 mb-4"
+      role="button" data-toggle="lightbox" data-max-width="80%" data-max-height="80%"
+      data-remote="@yield('hero_image')" data-title="@yield('hero_image_title')">
       <i class="fa fa-circle fa-stack-2x"></i>
       <i class="fa fa-expand fa-stack-1x fa-inverse"></i>
     </a>
