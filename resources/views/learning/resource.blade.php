2be97ff1251c7c6d4c4065166f27346ef240ef3b
Daniel Pett <dejp3@cam.ac.uk>
2020-06-11 18:55:42 +0100

    Amend layout for files

--- resources/views/learning/resource.blade.php
+++ resources/views/learning/resource.blade.php
@@ -14,15 +14,34 @@
   <h2>
     Downloadable resources
   </h2>
-    <div class="col-12 shadow-sm p-3 mx-auto mb-3 rounded ">
-      <ul>
-      @foreach($page['associated_learning_files'] as $files)
-        <li>
-          <a href="{{ $files['learning_files_id']['file']['data']['full_url']}}">{{ $files['learning_files_id']['title'] }}
-            @if(!is_null($files['learning_files_id']['keystages'])) - {{ implode(', ', $files['learning_files_id']['keystages']) }} @endif</a>
-      </li>
-      @endforeach
-      </ul>
+  @if(!empty($page['associated_learning_files']))
+  <h3>Factsheets and related files</h3>
+  <div class="row">
+
+
+    @foreach($page['associated_learning_files'] as $file)
+    <div class="col-md-4 mb-3">
+      <div class="card card-body h-100">
+        <div class="container h-100">
+          <div class="contents-label mb-3">
+            <h3>
+              {{ $file['learning_files_id']['title'] }}
+            </h3>
+            <ul>
+              <li>Resource type: {{ ucfirst($file['learning_files_id']['type']) }}</li>
+              <li>File size: @humansize($file['learning_files_id']['file']['filesize'],2)</li>
+              @if(isset($file['learning_files_id']['file']['type']))
+              <li>File type: PDF</li>
+              @endif
+            </ul>
+          </div>
+        </div>
+        <a href="{{ $file['learning_files_id']['file']['data']['url'] }}" class="btn btn-dark"><i class="fa fa-download mr-2" aria-hidden="true"></i>  Download file</a>
+      </div>
     </div>
+
+    @endforeach
+  </div>
+  @endif
   @endsection
 @endforeach
