d7916168a3c5a1f30a9cc7714a354d06332bc092
Daniel Pett <dejp3@cam.ac.uk>
2020-06-18 00:56:53 +0100

    Add 95%

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid head parallax position-relative">
-  <div class="absolute-bottom w-100">
+  <div class="absolute-bottom w-95">
     <div class="mr-3">
       <a tabindex="0" class="further-info fa-stack fa-lg float-right mr-4 mb-4" role="button" data-toggle="popover"
       data-placement="left" data-content="@yield('hero_image_title')">
@@ -8,7 +8,7 @@
     </div>
     <div class="mr-3">
       <a tabindex="0" class="further-info fa-stack fa-lg float-right mr-4 mb-4"
-      role="button" data-toggle="lightbox" data-max-width="1024" 
+      role="button" data-toggle="lightbox" data-max-width="1024"
       data-remote="@yield('hero_image')" data-title="@yield('hero_image_title')">
       <i class="fa fa-circle fa-stack-2x"></i>
       <i class="fa fa-expand fa-stack-1x fa-inverse"></i>
