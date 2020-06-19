652ab45a79861de16e5d3a985b721cb0fd25d819
Daniel Pett <dejp3@cam.ac.uk>
2020-06-19 08:16:57 +0100

    Fix route

--- resources/views/highlights/associate.blade.php
+++ resources/views/highlights/associate.blade.php
@@ -27,26 +27,26 @@
       <div class="col-md-4 mb-3">
         <div class="card card-body h-100">
           @if(!is_null($record['image']))
-            <img class="img-fluid" src="{{ $record['smallimage'][0]}}"
-            alt="Highlight image for {{ $record['title'][0] }}" loading="lazy"/>
+            <a href="/objects-and-artworks/highlights/context/{{ $record['section'][0]}}/{{ $record['slug'][0] }}"><img class="img-fluid" src="{{ $record['smallimage'][0]}}"
+            alt="Highlight image for {{ $record['title'][0] }}" loading="lazy"/></a>
           @endif
           <div class="container h-100">
             <div class="contents-label mb-3">
               @if(isset($record['section']))
                 <h3>
-                  <a href="/objects-and-artworks/highlights/{{ $record['section'][0]}}/{{ $record['slug'][0] }}">{{ $record['title'][0]}}</a>
+                  <a href="/objects-and-artworks/highlights/context/{{ $record['section'][0]}}/{{ $record['slug'][0] }}">{{ $record['title'][0]}}</a>
                 </h3>
               @else
-                <a href="/objects-and-artworks/highlights/{{ $record['slug'][0] }}">{{ $record['title'][0] }}</a>
+                <a href="/objects-and-artworks/highlights/context/{{ $record['slug'][0] }}">{{ $record['title'][0] }}</a>
               @endif
             </h3>
 
           </div>
         </div>
         @if(isset($record['section']))
-          <a href="/objects-and-artworks/highlights/{{ $record['section'][0]}}/{{ $record['slug'][0]}}" class="btn btn-dark">Read more</a>
+          <a href="/objects-and-artworks/highlights/context/{{ $record['section'][0]}}/{{ $record['slug'][0]}}" class="btn btn-dark">Read more</a>
         @else
-          <a href="/objects-and-artworks/highlights/{{ $record['slug'][0]}}" class="btn btn-dark">Read more</a>
+          <a href="/objects-and-artworks/highlights/context/{{ $record['slug'][0]}}" class="btn btn-dark">Read more</a>
         @endif
       </div>
 
