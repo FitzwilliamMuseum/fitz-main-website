16b71dfe52157d875b06635ee833d0eb21ead238
Daniel Pett <dejp3@cam.ac.uk>
2020-07-30 22:01:03 +0100

    Correct link

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -2,12 +2,12 @@
   <div class="row">
   <div class="col-md-6 mb-3 mt-3 mx-auto d-block">
     <div class="card card-body">
-      <a href="news/reopening-the-fitzwilliam-museum"><img class="img-fluid" src="https://content.fitz.ms/fitz-website/assets/opening soon.jpg?key=directus-large-contain" alt="Opening the Fitzwilliam banner"></a>
+      <a href="https://tickets.museums.cam.ac.uk"><img class="img-fluid" src="https://content.fitz.ms/fitz-website/assets/opening soon.jpg?key=directus-large-contain" alt="Opening the Fitzwilliam banner"></a>
       <div class="container h-100">
         <div class="contents-label mb-3">
         </div>
       </div>
-      <a href="news/reopening-the-fitzwilliam-museum" class="btn btn-dark">Book your ticket now</a>
+      <a href="https://tickets.museums.cam.ac.uk" class="btn btn-dark">Book your ticket now</a>
     </div>
   </div>
 </div>
