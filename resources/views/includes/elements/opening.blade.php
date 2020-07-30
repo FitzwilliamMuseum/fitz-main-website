2dbfd2884e518adfc9f9efd2f2f267563dfc9956
Daniel Pett <dejp3@cam.ac.uk>
2020-07-30 22:12:28 +0100

    Add good to go

--- resources/views/includes/elements/opening.blade.php
+++ resources/views/includes/elements/opening.blade.php
@@ -8,8 +8,7 @@
 <p>If you would like to borrow a Museum wheelchair, please also reserve this when you book your ticket.</p>
 <p>We will be reopening our exhibition <em>Feast &amp; Fast</em> until 31 August, and you will need to book tickets for an accompanied visit in groups of no more than 6 visitors.</p>
 <p>Visitors will be able to follow floor markings to help them move around the collection, and we are asking visitors to keep left and move in a clockwise direction, and to always keep two metres away from others. The length of an average visit is approximately 2 to 3 hours, during which time it is possible to see a wide range of objects in the collection.</p>
-<p>&nbsp;</p>
 <p>The Fitzwilliam Museum has been awarded the Visit Britain <em>We’re Good to Go</em> Industry Standard mark. This means we have followed government and industry COVID-19 guidelines, have a Risk Assessment in place and a process to maintain cleanliness and aid social distancing.</p>
-<p><img alt="Good to Go England logo" src="https://fitzmuseum.cam.ac.uk/sites/default/files/good-to-go-england.jpg" style="height:200px; width:200px">&nbsp;<br>
+<p><img alt="Good to Go England logo" src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/goodtogo.png"/><br>
 Naturally the situation may change at short notice, and we will be following the latest government guidelines at all times, so please check back here before your visit for the most up-to-date visitor information.</p>
-<p>For enquiries about existing bookings or if you do not have access to the internet to book admission tickets online please email:&nbsp;<a href="mailto:tickets@museums.cam.ac.uk">tickets@museums.cam.ac.uk</a> or telephone 01223 333 230 during normal museum opening hours.</p>
+<p>For enquiries about existing bookings or if you do not have access to the internet to book admission tickets online please email: <a href="mailto:tickets@museums.cam.ac.uk">tickets@museums.cam.ac.uk</a> or telephone 01223 333 230 during normal museum opening hours.</p>
