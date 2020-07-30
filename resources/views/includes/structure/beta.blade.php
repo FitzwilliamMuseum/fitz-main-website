d78c7ce7b8776c8d28c4ee4e84c4d65436dfe75d
Daniel Pett <dejp3@cam.ac.uk>
2020-07-30 22:02:41 +0100

    Update beta section

--- resources/views/includes/structure/beta.blade.php
+++ resources/views/includes/structure/beta.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid beta mb-4 pb-1">
   <div class="col-md-12">
     <p class="text-center share">
-      The Fitzwilliam Museum is currently closed due to <a href="/news/the-museum-and-coronavirus-covid-19-an-update">Covid-19</a>.<br />
+      The Fitzwilliam Museum is reopening to all on the 11th August.<br />
       <strong>&beta;</strong>: This is a Beta release of our potential new website. We want to co-design
       our Virtual Museum presence with you.<br /> You can return to our <a href="https://www.fitzmuseum.cam.ac.uk/">current site</a> at any time.
     </p>
