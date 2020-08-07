279c5c2301d8f4919b14607a66379b3fed7324e1
Daniel Pett <dejp3@cam.ac.uk>
2020-08-07 07:56:57 +0100

    Add height/width

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -2,7 +2,8 @@
   <div class="row">
   <div class="col-md-6 mb-3 mt-3 mx-auto d-block book">
     <div class="card card-body">
-      <a href="https://tickets.museums.cam.ac.uk"><img class="img-fluid" src="https://content.fitz.ms/fitz-website/assets/opening soon.jpg?key=directus-large-contain" alt="Opening the Fitzwilliam banner"></a>
+      <a href="https://tickets.museums.cam.ac.uk"><img class="img-fluid d-block mx-auto" src="https://content.fitz.ms/fitz-website/assets/opening soon.jpg?key=directus-large-contain" alt="Opening the Fitzwilliam banner"
+        height="432" width="1170"/></a>
       <div class="container h-100">
         <div class="contents-label mb-3">
         </div>
