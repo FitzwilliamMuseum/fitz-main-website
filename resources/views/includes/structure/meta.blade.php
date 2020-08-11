61c13756bb1dd91b17278a04bf96192935726923
Daniel Pett <dejp3@cam.ac.uk>
2020-08-11 23:59:24 +0100

    Added CSRF

--- resources/views/includes/structure/meta.blade.php
+++ resources/views/includes/structure/meta.blade.php
@@ -3,10 +3,11 @@
     <meta content="initial-scale=1, shrink-to-fit=no, width=device-width" name="viewport">
 
     <title>The Fitzwilliam Museum - @yield('title')</title>
+    
     <meta name="description" content="@yield('description')" />
-    @hasSection('keywords')
-      <meta name="keywords" content="@yield('keywords')" />
-    @endif
+@hasSection('keywords')
+    <meta name="keywords" content="@yield('keywords')" />
+@endif
     <!-- Canonical link -->
     <link rel="canonical" href="{{ URL::current() }}" />
 
@@ -18,13 +19,15 @@
     <meta property="og:url" content="{{ URL::current() }}" />
     <meta property="og:site_name" content="The Fitzwilliam Museum" />
 
-    @hasSection('social_thumbnail')
-      <meta property="og:image" content="@yield('hero_image')" />
-      <meta name="twitter:image" content="@yield('hero_image')" />
-    @else
-      <meta property="og:image" content="@yield('hero_image')" />
-      <meta name="twitter:image" content="@yield('hero_image')" />
-    @endif
+@hasSection('social_thumbnail')
+    <meta property="og:image" content="@yield('hero_image')" />
+    <meta name="twitter:image" content="@yield('hero_image')" />
+@else
+    <meta property="og:image" content="@yield('hero_image')" />
+    <meta name="twitter:image" content="@yield('hero_image')" />
+@endif
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Twitter card -->
     <meta name="twitter:card" content="summary" />
