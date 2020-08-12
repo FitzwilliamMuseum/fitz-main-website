15ae27f705d379c1f4b2baddf5fe4196a77379ca
Daniel Pett <dejp3@cam.ac.uk>
2020-08-12 07:41:58 +0100

    Remove opening from beya

--- resources/views/includes/structure/beta.blade.php
+++ resources/views/includes/structure/beta.blade.php
@@ -1,7 +1,6 @@
 <div class="container-fluid beta mb-4 pb-1">
   <div class="col-md-12">
     <p class="text-center share">
-      The Fitzwilliam Museum is reopening to all on the 11th August.<br />
       <strong>&beta;</strong>: This is a Beta release of our potential new website. We want to co-design
       our Virtual Museum presence with you.<br /> You can return to our <a href="https://www.fitzmuseum.cam.ac.uk/">current site</a> at any time.
     </p>
--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -2,7 +2,7 @@
   <div class="row">
   <div class="col-md-6 mb-3 mt-3 mx-auto d-block book">
     <div class="card card-body">
-      <a href="https://tickets.museums.cam.ac.uk"><img class="img-fluid d-block mx-auto" src="https://content.fitz.ms/fitz-website/assets/opening soon.jpg?key=directus-large-contain" alt="Opening the Fitzwilliam banner"
+      <a href="https://tickets.museums.cam.ac.uk"><img class="img-fluid d-block mx-auto" src="https://content.fitz.ms/fitz-website/assets/reopening_web_slide.jpg?key=directus-large-contain" alt="Book your ticket"
         height="432" width="1170"/></a>
       <div class="container h-100">
         <div class="contents-label mb-3">
