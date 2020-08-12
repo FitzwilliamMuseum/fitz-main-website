bd871922a21016c261399be08c5b8b9325fd3625
Daniel Pett <dejp3@cam.ac.uk>
2020-08-12 07:45:19 +0100

    Amend opening hours

--- resources/views/includes/elements/opening.blade.php
+++ resources/views/includes/elements/opening.blade.php
@@ -1,20 +1,26 @@
-<ul><li>When you arrive at the Fitzwilliam Museum, please enter through the Courtyard Entrance gates (the left side as you are looking at the front of the Museum) where our team will be there to welcome you. Your visit starts at the Courtyard Entrance and our <a href="https://www.fitzmuseum.cam.ac.uk/sites/default/files/Floor%20plan%20reopening.pdf">floor plan</a> will help guide you through the Museum. Nearly all our galleries are open, except those which are shown as closed on the plan.
-	 </li>
-<li>Please have either a paper printout of your ticket or be ready to show this on your smartphone for scanning.
-	 </li>
-<li>As you enter the Courtyard Entrance, a hand sanitising station is available, so please sanitise your hands.
-	 </li>
-<li>The only toilets on site will be the ones in the basement level at the Courtyard Entrance (the ones at the Founder’s Entrance are not in use) so we recommend you visit the facilities at the Courtyard Entrance at the start or end of your visit.
-	 </li>
-<li>Our regular visitors may notice some differences to what is on offer to families. Currently Fitz Kits, activity boxes, story starters, drawing materials and our baby play mat are not available to borrow. Free family trails continue to be available. We suggest bringing your own paper and pencils if you want to draw in the gallery.
-	 </li>
-<li>Visitors will be able to follow floor markings to help them move around the collection, and we are asking visitors to keep left and move in a clockwise direction.
-	 </li>
-<li>You will be asked to observe social distancing from others throughout the duration of your visit, and you may be asked to wait until others have moved on before proceeding into the next room.
-	 </li>
-<li>To reduce risks for all our visitors, large print label books have currently been removed from the galleries. An online version of the Large Label books will be available soon.
-	 </li>
-<li>If you feel unwell at any stage during your visit, please alert a member of staff immediately so we can assist you.
-	 </li>
-<li>After your visit, you can exit the museum using the Courtyard Entrance, although if you wish you can also exit from the Main Entrance in the Founder’s Building. The recent pandemic has profoundly impacted the economic situation of the cultural sector, The Fitzwilliam Museum included. If you are able, please consider making a donation, becoming a Friend or joining the Marlay Group. Your support, of any amount, is enormously appreciated as it will help to secure our future and enable us to continue to offer: free education sessions, inspiring exhibitions, world-class research, exemplary collections care and free access to our galleries. <a href="http://tickets.museums.cam.ac.uk/donate/i/donate-to-the-fitzwilliam">Donate today</a> – your support has never been more important. Thank you.</li>
+<p>
+	Tuesday - Saturday: 10:00 - 17:00<br/>
+	Sundays and Bank Holiday Mondays: 12:00 - 17:00<br/>
+	CLOSED: Mondays, Good Friday, 24-26 & 31 December and 1 January
+</p>
+<ul>
+	<li>When you arrive at the Fitzwilliam Museum, please enter through the Courtyard Entrance gates (the left side as you are looking at the front of the Museum) where our team will be there to welcome you. Your visit starts at the Courtyard Entrance and our <a href="https://www.fitzmuseum.cam.ac.uk/sites/default/files/Floor%20plan%20reopening.pdf">floor plan</a> will help guide you through the Museum. Nearly all our galleries are open, except those which are shown as closed on the plan.
+	</li>
+	<li>Please have either a paper printout of your ticket or be ready to show this on your smartphone for scanning.
+	</li>
+	<li>As you enter the Courtyard Entrance, a hand sanitising station is available, so please sanitise your hands.
+	</li>
+	<li>The only toilets on site will be the ones in the basement level at the Courtyard Entrance (the ones at the Founder’s Entrance are not in use) so we recommend you visit the facilities at the Courtyard Entrance at the start or end of your visit.
+	</li>
+	<li>Our regular visitors may notice some differences to what is on offer to families. Currently Fitz Kits, activity boxes, story starters, drawing materials and our baby play mat are not available to borrow. Free family trails continue to be available. We suggest bringing your own paper and pencils if you want to draw in the gallery.
+	</li>
+	<li>Visitors will be able to follow floor markings to help them move around the collection, and we are asking visitors to keep left and move in a clockwise direction.
+	</li>
+	<li>You will be asked to observe social distancing from others throughout the duration of your visit, and you may be asked to wait until others have moved on before proceeding into the next room.
+	</li>
+	<li>To reduce risks for all our visitors, large print label books have currently been removed from the galleries. An online version of the Large Label books will be available soon.
+	</li>
+	<li>If you feel unwell at any stage during your visit, please alert a member of staff immediately so we can assist you.
+	</li>
+	<li>After your visit, you can exit the museum using the Courtyard Entrance, although if you wish you can also exit from the Main Entrance in the Founder’s Building. The recent pandemic has profoundly impacted the economic situation of the cultural sector, The Fitzwilliam Museum included. If you are able, please consider making a donation, becoming a Friend or joining the Marlay Group. Your support, of any amount, is enormously appreciated as it will help to secure our future and enable us to continue to offer: free education sessions, inspiring exhibitions, world-class research, exemplary collections care and free access to our galleries. <a href="http://tickets.museums.cam.ac.uk/donate/i/donate-to-the-fitzwilliam">Donate today</a> – your support has never been more important. Thank you.</li>
 </ul>
