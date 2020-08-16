06621bed87dc988221710d3e3b6f4e53e0c97664
Daniel Pett <dejp3@cam.ac.uk>
2020-08-16 23:03:45 +0100

    Remove linkedin share

--- resources/views/includes/structure/share.blade.php
+++ resources/views/includes/structure/share.blade.php
@@ -3,9 +3,8 @@
     <div class="col-md-6 mx-auto">
     <div class="social_box">
         <ul class="text-center">
-            <li><a href="https://www.facebook.com/sharer/sharer.php?u={{ URL::current() }}%2F"><i class="fab fa-facebook fa-2x p-2"></i></a></li>
-            <li><a href="https://twitter.com/intent/tweet?url={{ URL::current() }}%2F&text="><i class="fab fa-twitter fa-2x p-2"></i></a></li>
-            <li><a href="https://www.linkedin.com/shareArticle?url={{ URL::current() }}k%2F&title=&mini=true"><i class="fab fa-linkedin fa-2x p-2"></i></a></li>
+            <li><a href="https://www.facebook.com/sharer/sharer.php?u={{ URL::current() }}"><i class="fab fa-facebook fa-2x p-2"></i></a></li>
+            <li><a href="https://twitter.com/intent/tweet?url={{ URL::current() }}%2F&text=@yield('title')"><i class="fab fa-twitter fa-2x p-2"></i></a></li>
         </ul>
       </div>
     </div>
