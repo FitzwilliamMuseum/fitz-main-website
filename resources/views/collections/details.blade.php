e9d3cdf99f266617cf0172930d9aff6144da080e
Daniel Pett <dejp3@cam.ac.uk>
2020-08-16 20:13:03 +0100

    Update collections cards

--- resources/views/collections/details.blade.php
+++ resources/views/collections/details.blade.php
@@ -20,10 +20,10 @@
             <div class="card  h-100">
               @if(!is_null($gallery['departments_id']['hero_image']))
               <div class="embed-responsive embed-responsive-1by1">
-                <img class="img-fluid embed-responsive-item" src="{{ $gallery['departments_id']['hero_image']['data']['thumbnails'][4]['url']}}"
+                <a href="/departments/{{ $gallery['departments_id']['slug']}}"><img class="img-fluid embed-responsive-item" src="{{ $gallery['departments_id']['hero_image']['data']['thumbnails'][4]['url']}}"
                 width="{{ $gallery['departments_id']['hero_image']['data']['thumbnails'][4]['width']}}"
                 height="{{ $gallery['departments_id']['hero_image']['data']['thumbnails'][4]['height']}}"
-                alt="{{ $gallery['departments_id']['hero_image_alt_text'] }}" loading="lazy"/>
+                alt="{{ $gallery['departments_id']['hero_image_alt_text'] }}" loading="lazy"/></a>
               </div>
               @endif
               <div class="card-body">
@@ -48,22 +48,23 @@
       <div class="row">
         @foreach($coll['associated_galleries'] as $gallery)
         <div class="col-md-4 mb-3">
-          <div class="card card-body h-100">
+          <div class="card">
             @if(!is_null($gallery['galleries_id']['hero_image']))
-            <img class="img-fluid" src="{{ $gallery['galleries_id']['hero_image']['data']['thumbnails'][4]['url'] }}"
+            <div class="embed-responsive embed-responsive-1by1">
+            <a href="/galleries/{{ $gallery['galleries_id']['slug']}}"><img class="embed-responsive-item img-fluid" src="{{ $gallery['galleries_id']['hero_image']['data']['thumbnails'][4]['url'] }}"
             width="{{ $gallery['galleries_id']['hero_image']['data']['thumbnails'][4]['width'] }}"
             height="{{ $gallery['galleries_id']['hero_image']['data']['thumbnails'][4]['height'] }}"
             alt="{{ $gallery['galleries_id']['hero_image_alt_text'] }}" loading="lazy"
-            />
+            /></a>
+            </div>
             @endif
-            <div class="container h-100">
+            <div class="card-body  h-100">
               <div class="contents-label mb-3">
                 <h3>
                   <a href="/galleries/{{ $gallery['galleries_id']['slug']}}">{{ $gallery['galleries_id']['gallery_name']}}</a>
                 </h3>
               </div>
             </div>
-            <a href="/galleries/{{ $gallery['galleries_id']['slug']}}" class="btn btn-dark">Read more</a>
           </div>
         </div>
         @endforeach
