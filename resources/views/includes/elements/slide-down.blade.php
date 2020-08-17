0def3e6b150730f832551cffb7c0f449ca228a28
Daniel Pett <dejp3@cam.ac.uk>
2020-08-17 22:43:27 +0100

    Amend footers and logos

---
+++ resources/views/includes/elements/slide-down.blade.php
@@ -0,0 +1,26 @@
+<div class="site-notification site-notification_hours" style="height: 313px;">
+		<div class="site-notification_close"></div>
+		<div class="container">
+							                  <div class="col col_4">
+            <div class="heading--tertiary">Opening Hours</div>
+            <p>House, galleries, café and shop: Friday – Sunday 11am – 5pm</p>
+<div class="page-content_section">
+<p>Free, timed entry tickets to the House and galleries will be available online from 5 August.</p>
+<p>Last entry to the House is at 4.20pm</p>
+</div>
+          </div>
+                  <div class="col col_4">
+            <div class="heading--tertiary">Access Information &amp; Contact Us</div>
+            <p><a href="https://www.kettlesyard.co.uk/access/" target="_blank" rel="noopener">Find access information here.&nbsp;</a></p>
+<p>+44 (0)1223 748 100<br>
+<a href="mailto:mail@kettlesyard.cam.ac.uk">mail@kettlesyard.cam.ac.uk</a></p>
+<p>&nbsp;</p>
+          </div>
+                  <div class="col col_4 col_last">
+            <div class="heading--tertiary">Kettle’s Yard News</div>
+            <p>Be the first to hear our latest news by <a href="http://kettlesyard.us6.list-manage.com/subscribe?u=f0716a076921fc351692005c3&amp;id=3208092643" target="_blank" rel="noopener">signing up to our mailing list</a>.</p>
+<p>For our latest blogs&nbsp;<a href="https://www.kettlesyard.co.uk/about/blog/" target="_blank" rel="noopener">click here</a></p>
+<p>Find out <a href="https://www.kettlesyard.co.uk/events/" target="_blank" rel="noopener">What’s On at Kettle’s Yard here.</a></p>
+          </div>
+                  </div>
+	</div>
