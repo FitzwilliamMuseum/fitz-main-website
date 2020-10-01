8cee65aed81a9f5ec82fdf09e37fc6afb21d9a3c
Daniel Pett <dejp3@cam.ac.uk>
2020-10-01 13:20:36 +0100

    Add insta

--- resources/views/includes/structure/share.blade.php
+++ resources/views/includes/structure/share.blade.php
@@ -6,6 +6,7 @@
           <div>Start a conversation?</div>
             <li><a href="https://www.facebook.com/sharer/sharer.php?u={{ URL::current() }}"><i class="fab fa-facebook fa-2x p-2"></i></a></li>
             <li><a href="https://twitter.com/intent/tweet?url={{ URL::current() }}%2F&text=@yield('title')"><i class="fab fa-twitter fa-2x p-2"></i></a></li>
+            <li><a href="https://instagram.com"><i class="fab fa-instagram fa-2x p-2"></i></a></li>
         </ul>
       </div>
     </div>
