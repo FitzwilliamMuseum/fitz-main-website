d2876bc0066e609cee9644faaff74bdd2e8901da
Daniel Pett <dejp3@cam.ac.uk>
2020-10-05 23:29:03 +0100

    Added holding page

--- resources/views/podcasts/mindseyes.blade.php
+++ resources/views/podcasts/mindseyes.blade.php
@@ -6,10 +6,25 @@
 @section('content')
 
 <div class="row">
+  <div class="col-md-4 mb-3">
+    <div class="card h-100">
+      <a href=""><img class="img-fluid" src="https://content.fitz.ms/fitz-website/assets/mindseye.jpg?key=directus-large-crop&q=50"
+      alt=""
+
+      loading="lazy"/></a>
+      <div class="card-body h-100">
+        <div class="contents-label mb-3">
+          <h3>
+            In my mind's eye: culture in quarantine podcasts
+          </h3>
+        </div>
+      </div>
+    </div>
+  </div>
   @foreach($mindseyes['data'] as $instagram)
   <div class="col-md-4 mb-3">
     <div class="card h-100">
-      <a href="{{ route('instagram.story', $instagram['slug']) }}"><img class="img-fluid" src="{{ $instagram['hero_image']['data']['thumbnails'][4]['url']}}"
+      <a href="{{ route('mindeyes.story', $instagram['slug']) }}"><img class="img-fluid" src="{{ $instagram['hero_image']['data']['thumbnails'][4]['url']}}"
       alt="{{ $instagram['hero_image_alt_text'] }}"
       width="{{ $instagram['hero_image']['data']['thumbnails'][4]['width'] }}"
       height="{{ $instagram['hero_image']['data']['thumbnails'][4]['height'] }}"
@@ -24,6 +39,21 @@
     </div>
   </div>
   @endforeach
+  <div class="col-md-4 mb-3">
+    <div class="card h-100">
+      <a href=""><img class="img-fluid" src="https://content.fitz.ms/fitz-website/assets/cover-podcasts.jpg?key=directus-large-crop&q=50"
+      alt=""
+
+      loading="lazy"/></a>
+      <div class="card-body h-100">
+        <div class="contents-label mb-3">
+          <h3>
+            Listen to more Fitzwilliam Museum podcasts
+          </h3>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 
