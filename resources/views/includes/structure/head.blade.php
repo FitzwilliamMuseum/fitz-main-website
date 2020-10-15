4ccd58b5e271d9de4a6caa8767bd37a0b4422533
Daniel Pett <dejp3@cam.ac.uk>
2020-10-15 19:11:25 +0100

    Add svg

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid head parallax ">
-
+    <img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fv-logo-white.svg" class="img-fluid" width="500">
 </div>
 <div class="container-fluid bg-white text-black text-center">
   <div class="ml-3 pt-4 pb-4">
