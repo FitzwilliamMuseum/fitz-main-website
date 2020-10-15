29d2d81b3ddb16fb5c2f0e4885761536660513f5
Daniel Pett <dejp3@cam.ac.uk>
2020-10-16 00:52:42 +0100

    Beta message add FM

--- resources/views/includes/structure/beta.blade.php
+++ resources/views/includes/structure/beta.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid beta pb-1">
   <div class="col-md-12">
     <p class="text-center share">
-      <strong>&beta;</strong>: This is a Beta release of our potential new website. We want to <a href="https://co-create.fitz.ms/">co-design</a>
+      <strong>&beta;</strong>: This is a Beta release of the Fitzwilliam Museum's potential new website.<br/> We want to <a href="https://co-create.fitz.ms/">co-design</a>
       our Virtual Museum presence with you.<br /> You can return to our <a href="https://www.fitzmuseum.cam.ac.uk/">current site</a> at any time.
     </p>
   </div>
