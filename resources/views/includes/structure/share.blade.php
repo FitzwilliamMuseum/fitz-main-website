6401d72b1bce5c01f5718f4451e514eb0bdf5650
Daniel Pett <dejp3@cam.ac.uk>
2020-10-16 11:06:19 +0100

    Remove instagram

--- resources/views/includes/structure/share.blade.php
+++ resources/views/includes/structure/share.blade.php
@@ -2,12 +2,10 @@
   <div class="col-md-12 shadow-sm p-2 ">
     <div class="col-md-6 mx-auto">
     <div class="social_box">
-      <h4 class="text-center lead">Start a conversation</h4>
+      <h4 class="text-center lead">Join our conversation</h4>
         <ul class="text-center conversation-footer">
-
             <li><a href="https://www.facebook.com/sharer/sharer.php?u={{ URL::current() }}"><i class="fab fa-facebook fa-2x p-2"></i></a></li>
             <li><a href="https://twitter.com/intent/tweet?url={{ URL::current() }}%2F&text=@yield('title')"><i class="fab fa-twitter fa-2x p-2"></i></a></li>
-            <li><a href="https://instagram.com"><i class="fab fa-instagram fa-2x p-2"></i></a></li>
         </ul>
       </div>
     </div>
