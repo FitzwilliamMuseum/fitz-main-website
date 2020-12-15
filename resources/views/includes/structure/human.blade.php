eac5a746e4e52cd5eeaa941d2831494d851d7011
Daniel Pett <dejp3@cam.ac.uk>
2020-12-15 11:14:55 +0000

    Add correct attribute

--- resources/views/includes/structure/human.blade.php
+++ resources/views/includes/structure/human.blade.php
@@ -3,7 +3,7 @@
   <video id="contentVideo" autoplay="true" muted="true" loop="true" src="https://human-touch.beta.fitz.ms/tease.mp4"><source src="https://human-touch.beta.fitz.ms/tease.mp4" type="video/mp4">Your device does not support playing 'video/mp4' videos</video>
   </div>
   <div class="humancontent mt-5  text-center">
-    <h3 class="hc"><a hred="https://human-touch.beta.fitz.ms">The Human Touch</a></h3>
+    <h3 class="hc"><a href="https://human-touch.beta.fitz.ms">The Human Touch</a></h3>
     <p class="lead ">Making Art | Leaving Traces</p>
     <button type="button" class="btn btn-btnBook mt-2 mb-2">
       <a href="https://tickets.museums.cam.ac.uk/overview/thehumantouch">Book now</a>
