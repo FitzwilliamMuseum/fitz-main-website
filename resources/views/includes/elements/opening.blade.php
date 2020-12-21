b80301eadf144a25874912ed61c864adaf3a3b9f
Daniel Pett <dejp3@cam.ac.uk>
2020-12-21 11:32:30 +0000

    Change visiting info

--- resources/views/includes/elements/opening.blade.php
+++ resources/views/includes/elements/opening.blade.php
@@ -1,4 +1,12 @@
 <p>
+	Following revised government guidelines, please do not visit the museum if
+	you are resident in a Tier 3 or Tier 4 area. You can cancel or reschedule
+	your booking by emailing
+	<a href="mailto:tickets@museums.cam.ac.uk?subject=Tier3/4">tickets@museums.cam.ac.uk</a> or calling 01223 333230 
+	during our opening hours. We look forward to welcoming you to the museum
+	another time.
+</p>
+<p>
 	Tuesday - Saturday: 10:00 - 17:00<br/>
 	Sundays and Bank Holiday Mondays: 12:00 - 17:00<br/>
 	CLOSED: Mondays, Good Friday, 24-26 & 31 December and 1 January
