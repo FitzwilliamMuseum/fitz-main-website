05c89bbcce4ba4d0ed4217f97d144c69066fb1ed
Daniel Pett <dejp3@cam.ac.uk>
2020-12-24 00:47:28 +0000

    Change opening message

--- resources/views/includes/elements/opening.blade.php
+++ resources/views/includes/elements/opening.blade.php
@@ -1,13 +1,3 @@
 <p>
-	Following revised government guidelines, please do not visit the museum if
-	you are resident in a Tier 3 or Tier 4 area. You can cancel or reschedule
-	your booking by emailing
-	<a href="mailto:tickets@museums.cam.ac.uk?subject=Tier3/4">tickets@museums.cam.ac.uk</a> or calling 01223 333230
-	during our opening hours. We look forward to welcoming you to the museum
-	another time.
-</p>
-<p>
-	Wednesday - Saturday: 10:00 - 17:00<br/>
-	Sundays and Bank Holiday Mondays: 12:00 - 17:00<br/>
-	CLOSED: Mondays, Good Friday, 24th December to the 1st January
+	As Cambridgeshire will be in tier 4 from 00:01 on Boxing Day, the Museum will be closed for the foreseeable future. 
 </p>
--- resources/views/includes/structure/open.blade.php
+++ resources/views/includes/structure/open.blade.php
@@ -1,9 +1,10 @@
 <div class="container-fluid bg-white p-1">
   <p class="text-center text-black">
-    @fa('ticket-alt', 'ticket', 'mr-2') <a class="free_ticket" href="https://tickets.museums.cam.ac.uk/overview/generaladmission">FREE ENTRY- Book a ticket</a><br/>
+    {{-- @fa('ticket-alt', 'ticket', 'mr-2') <a class="free_ticket" href="https://tickets.museums.cam.ac.uk/overview/generaladmission">FREE ENTRY- Book a ticket</a><br/>
     Wednesday - Saturday: 10:00 - 17:00 |
     Sundays and Bank Holiday Mondays: 12:00 - 17:00<br />
     Closed 24th December - 1st January inclusive.
-    @fa('door-closed', 'closed', 'mr-2') <a class="free_ticket" href="{{ URL::to('/visit-us/gallery-closures-and-collection-updates') }}" >Gallery closures and collection updates</a>
+    @fa('door-closed', 'closed', 'mr-2') <a class="free_ticket" href="{{ URL::to('/visit-us/gallery-closures-and-collection-updates') }}" >Gallery closures and collection updates</a> --}}
+    As Cambridgeshire will be in tier 4 from 00:01 on Boxing Day, the Museum will be closed for the foreseeable future. 
   </p>
 </div>
