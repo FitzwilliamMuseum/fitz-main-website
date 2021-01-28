3460023a6b6f470a040458f194193e2819bc7722
Daniel Pett <dejp3@cam.ac.uk>
2021-01-28 00:56:58 +0000

    Update THT

--- resources/views/includes/elements/opening.blade.php
+++ resources/views/includes/elements/opening.blade.php
@@ -1,2 +1,4 @@
 <p>
-Following the Government announcement yesterday, museums and galleries in Cambridge will be closed to the public as part of a period of national/local restrictions. So, with great sadness, we will not be able to reopen as planned on 2 January 2021.  </p>
+Museums and galleries in Cambridge will be closed to the public as part of a period of national/local 
+restrictions.
+</p>
--- resources/views/includes/structure/human.blade.php
+++ resources/views/includes/structure/human.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid human" id="coverVideo">
+<div class="container-fluid human bg-black" id="coverVideo">
   <div class="vid">
   <video id="contentVideo" autoplay="true" muted="true" loop="true" src="https://human-touch.beta.fitz.ms/tease.mp4"><source src="https://human-touch.beta.fitz.ms/tease.mp4" type="video/mp4">Your device does not support playing 'video/mp4' videos</video>
   </div>
@@ -8,6 +8,5 @@
     <button type="button" class="btn btn-btnBook mt-2 mb-2">
       <a href="https://tickets.museums.cam.ac.uk/overview/thehumantouch">Book now</a>
     </button>
-    <p class="lead">6 January 2021 to 3 May 2021</p>
   </div>
 </div>
