f3dae480792c84b859ab8f32a12f7981b35aa332
Daniel Pett <dejp3@cam.ac.uk>
2021-03-08 10:01:26 +0000

    Add tickets back

--- resources/views/includes/structure/nav.blade.php
+++ resources/views/includes/structure/nav.blade.php
@@ -23,7 +23,9 @@
         <a class="dropdown-item" href="{{ URL::to('/objects-and-artworks/image-library/') }}">Image library</a>
       </div>
     </li>
-
+    <li class="nav-item active">
+      <a class="nav-link" href="https://tickets.museums.cam.ac.uk">Events and tickets</a>
+    </li>
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownVisit" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Visit</a>
       <div class="dropdown-menu" aria-labelledby="navbarDropdownVisit">
