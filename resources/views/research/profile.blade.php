f26889319deffbc1b38590a65acccc25f99676c5
Daniel Pett <dejp3@cam.ac.uk>
2021-03-17 22:06:57 +0000

    Fix bug with syntax

--- resources/views/research/profile.blade.php
+++ resources/views/research/profile.blade.php
@@ -50,9 +50,10 @@
       <h3>Associated Research Projects</h3>
       <div class="row">
         @foreach($profile['research_projects'] as $project)
+          @if(!is_null($project['research_projects_id']))
         <div class="col-md-4 mb-3">
           <div class="card  h-100 ">
-            @if(!is_null($project['research_projects_id']['hero_image']))
+
             <div class="embed-responsive embed-responsive-4by3">
               <a href="{{ route('research-project', $project['research_projects_id']['slug']) }}"><img class="img-fluid embed-responsive-item" src="{{ $project['research_projects_id']['hero_image']['data']['thumbnails'][4]['url']}}"
               width="{{ $project['research_projects_id']['hero_image']['data']['thumbnails'][4]['width'] }}"
@@ -60,7 +61,7 @@
               alt="{{ $project['research_projects_id']['hero_image_alt_text'] }}"
               loading="lazy"/></a>
             </div>
-            @endif
+
             <div class="card-body">
               <div class="contents-label mb-3">
                 <h3>
@@ -68,8 +69,10 @@
                 </h3>
               </div>
             </div>
+
           </div>
         </div>
+        @endif
         @endforeach
       </div>
     </div>
@@ -109,33 +112,5 @@
     @endsection
   @endif
 
-  @if(!empty($profile['research_projects']))
-    @section('research-projects')
-    <div class="container">
-      <h3>Associated Research Projects</h2>
-      <div class="row">
-        @foreach($profile['research_projects'] as $project)
-        <div class="col-md-6 mb-3">
-          <div class="card">
-            @if(!is_null($project['research_projects_id']['hero_image']))
-              <a href="{{ route('research-project', $project['research_projects_id']['slug']) }}"><img class="img-fluid" src="{{ $project['research_projects_id']['hero_image']['data']['thumbnails'][4]['url']}}"
-              alt="{{ $project['research_projects_id']['hero_image_alt_text'] }}"
-              height="{{ $project['research_projects_id']['hero_image']['data']['thumbnails'][4]['height'] }}"
-              width="{{ $project['research_projects_id']['hero_image']['data']['thumbnails'][4]['width'] }}"
-              loading="lazy"/></a>
-            @endif
-            <div class="card-body">
-              <div class="contents-label mb-3">
-                <h3>
-                  <a href="{{ route('research-project', $project['research_projects_id']['slug']) }}">{{ $project['research_projects_id']['title']}}</a>
-                </h3>
-              </div>
-            </div>
-          </div>
-        </div>
-        @endforeach
-      </div>
-    </div>
-    @endsection
-  @endif
+  
 @endforeach
