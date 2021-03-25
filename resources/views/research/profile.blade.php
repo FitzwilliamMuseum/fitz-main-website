8e746431edeb0b18b052b583a83e3d7f58510fea
Daniel Pett <dejp3@cam.ac.uk>
2021-03-25 14:18:53 +0000

    Layout changes

--- resources/views/research/profile.blade.php
+++ resources/views/research/profile.blade.php
@@ -21,18 +21,30 @@
         <li>{{ $profile['college_affiliated'] }}</li>
       </ul>
       @endif
-      @if(isset($profile['orcid']) || isset($profile['google_scholar_id']) || isset($profile['githubid']))
-      <h4>Research profiles</h4>
+      @if(isset($profile['orcid']) || isset($profile['google_scholar_id']))
+      <h4>
+        Research profiles
+      </h4>
       <ul>
+
         @if(isset($profile['orcid'])
-        <li><a href="https://orcid.org/{{$profile['orcid']}}">ORCID</a></li>
+          <li>
+            <a href="https://orcid.org/{{$profile['orcid']}}">ORCID</a>
+          </li>
         @endif
+
         @if(isset($profile['google_scholar_id'])
-        <li><a href="https://scholar.google.com/citations?user={{ $profile['google_scholar_id']}}">Google Scholar</a></li>
+          <li>
+            <a href="https://scholar.google.com/citations?user={{ $profile['google_scholar_id']}}">Google Scholar</a>
+          </li>
         @endif
+
         @if(isset($profile['githubid'])
-        <li><a href="https://github.com/{{ $profile['githubid']}}">Github</a></li>
+          <li>
+            <a href="https://github.com/{{ $profile['githubid']}}">Github</a>
+          </li>
         @endif
+
       </ul>
       @endif
     </div>
