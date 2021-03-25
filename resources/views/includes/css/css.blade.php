23126307760882ce809594a76876f9d894b0eac3
Daniel Pett <dejp3@cam.ac.uk>
2021-03-25 23:57:54 +0000

    Change fonts

--- resources/views/includes/css/css.blade.php
+++ resources/views/includes/css/css.blade.php
@@ -1,5 +1,6 @@
 <!-- CSS -->
-<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Arbutus+Slab&family=Rubik&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
 integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
