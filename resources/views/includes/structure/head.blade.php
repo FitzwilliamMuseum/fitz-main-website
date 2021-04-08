d022d054cc01dac8b167126d8ebf0dcb25e15cf1
Daniel Pett <dejp3@cam.ac.uk>
2021-04-09 00:41:34 +0100

    Add rounded, height adjust div

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -1,6 +1,5 @@
 <div class="container-fluid head parallax">
-      <img src="/images/logos/Fitz_logo_white.png" alt="The Fitzwilliam Museum Logo" height="200" class=" ml-5 logo p-4 bg-dark" />
-    {{-- <img src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fv-logo-white.svg" alt="The Fitzwilliam Museum Virtual logo" class="img-fluid mt-5" width="400"> --}}
+      <img src="/images/logos/Fitz_logo_white.png" alt="The Fitzwilliam Museum Logo" height="200" class=" ml-5 logo p-4 bg-dark rounded" />
 </div>
 <div class="container-fluid bg-white text-black text-center">
   <div class="ml-3 pt-4 pb-4">
