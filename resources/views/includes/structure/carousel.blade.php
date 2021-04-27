75c681fc955495ff9c907043d8d51536b7aa9d18
Daniel Pett <dejp3@cam.ac.uk>
2021-04-28 00:35:17 +0100

    Add better urls

--- resources/views/includes/structure/carousel.blade.php
+++ resources/views/includes/structure/carousel.blade.php
@@ -1,29 +1,29 @@
-
+{{-- @dd($carousel['data']) --}}
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner carousel-front">
     <div class="carousel-item active" >
-      <img class="d-block w-100" src="{{ $carousel['data']['image_one']['data']['thumbnails'][3]['url']}}"
+      <img class="d-block w-100" src="{{ $carousel['data']['image_one']['data']['thumbnails'][9]['url']}}"
       alt="{{ $carousel['data']['image_one_alt_text'] }}" loading="lazy" />
       <div class="carousel-caption d-none d-md-block bg-black p-3">
         <h5>{{ $carousel['data']['image_one_alt_text'] }}</h5>
       </div>
     </div>
     <div class="carousel-item" >
-      <img class="d-block w-100" src="{{ $carousel['data']['image_two']['data']['thumbnails'][5]['url']}}"
+      <img class="d-block w-100" src="{{ $carousel['data']['image_two']['data']['thumbnails'][9]['url']}}"
       alt="{{ $carousel['data']['image_two_alt_text']}}" />
       <div class="carousel-caption d-none d-md-block bg-black p-3">
         <h5>{{ $carousel['data']['image_two_alt_text']}}</h5>
       </div>
     </div>
     <div class="carousel-item" >
-      <img class="d-block w-100" src="{{ $carousel['data']['image_three']['data']['thumbnails'][3]['url']}}"
+      <img class="d-block w-100" src="{{ $carousel['data']['image_three']['data']['thumbnails'][9]['url']}}"
       alt="{{ $carousel['data']['image_three_alt_text'] }}"  />
       <div class="carousel-caption d-none d-md-block bg-black p-3">
         <h5>{{ $carousel['data']['image_three_alt_text'] }}</h5>
       </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="{{ $carousel['data']['image_four']['data']['thumbnails'][3]['url']}}"
+      <img class="d-block w-100" src="{{ $carousel['data']['image_four']['data']['thumbnails'][9]['url']}}"
       alt="{{ $carousel['data']['image_four_alt_text'] }}" />
       <div class="carousel-caption d-none d-md-block bg-black p-3">
         <h5>{{ $carousel['data']['image_four_alt_text'] }}</h5>
