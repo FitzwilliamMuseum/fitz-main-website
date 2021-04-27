839a2229be993d12545abc4eee97ea5d280860e9
Daniel Pett <dejp3@cam.ac.uk>
2021-04-28 00:07:20 +0100

    add image dims

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid head parallax">
-      <img src="/images/logos/Fitz_logo_white.png" alt="The Fitzwilliam Museum Logo" height="200" class=" ml-5 logo p-4 bg-dark rounded" />
+      <img src="/images/logos/Fitz_logo_white.png" alt="The Fitzwilliam Museum Logo" height="200" width="216.84" class=" ml-5 logo p-4 bg-dark rounded" />
 </div>
 <div class="container-fluid bg-white text-black text-center">
   <div class="ml-3 pt-4 pb-4">
