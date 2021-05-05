b8d71e08096945c9a9cf98a0f945a7bee8c06736
Daniel Pett <dejp3@cam.ac.uk>
2021-05-05 16:34:32 +0100

    Change wording

--- resources/views/exhibitions/details.blade.php
+++ resources/views/exhibitions/details.blade.php
@@ -316,7 +316,7 @@
     @if(!empty($records))
       @section('mlt')
         <div class="container">
-          <h3>Similar exhibitions</h3>
+          <h3>Similar exhibitions from our archives</h3>
           <div class="row">
             @foreach($records as $record)
               <div class="col-md-4 mb-3">
