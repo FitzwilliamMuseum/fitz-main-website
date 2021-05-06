2a4f166a76afd068584622da24d66ff0d56ddf99
Daniel Pett <dejp3@cam.ac.uk>
2021-05-06 17:21:05 +0100

    Add tuesday

--- resources/views/includes/elements/opening.blade.php
+++ resources/views/includes/elements/opening.blade.php
@@ -1,7 +1,7 @@
 <p>
   From the 18th May:
   @fa('ticket-alt', 'ticket', 'mr-2') <a class="free_ticket" href="https://tickets.museums.cam.ac.uk/overview/generaladmission">FREE ENTRY- Book a ticket</a><br/>
-  Wednesday - Saturday: 10:00 - 17:00 |
+  Tuesday - Saturday: 10:00 - 17:00 |
   Sundays and Bank Holiday Mondays: 12:00 - 17:00<br />
   Closed 24th December - 1st January inclusive.
   @fa('door-closed', 'closed', 'mr-2') <a class="free_ticket" href="{{ URL::to('/visit-us/gallery-closures-and-collection-updates') }}" >Gallery closures and collection updates</a>
