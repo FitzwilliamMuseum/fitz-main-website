8cd4063cede94641a2c3dafedfc4fdd58b6941f3
Daniel Pett <dejp3@cam.ac.uk>
2021-05-06 01:09:01 +0100

    Remove ekko

--- resources/views/includes/scripts/javascript.blade.php
+++ resources/views/includes/scripts/javascript.blade.php
@@ -4,14 +4,6 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script><!-- Back to top script -->
 <script src="/js/backtotop.js"></script>
-<!-- lightbox -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js"></script>
-<script>
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
-});
-</script>
 
 @hasSection('theme-carousel')
   <script src="/js/carousel-themes.js"></script>
