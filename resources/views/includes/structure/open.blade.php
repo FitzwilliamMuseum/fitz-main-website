fc42ababfdd4a781d5ccf378dfb33a05731db243
Daniel Pett <dejp3@cam.ac.uk>
2021-05-06 17:11:33 +0100

    Adjust opening

--- resources/views/includes/structure/open.blade.php
+++ resources/views/includes/structure/open.blade.php
@@ -1,13 +1,14 @@
 <div class="container-fluid bg-white p-1">
   <div class="container">
     <p class="text-center text-black">
-      {{-- @fa('ticket-alt', 'ticket', 'mr-2') <a class="free_ticket" href="https://tickets.museums.cam.ac.uk/overview/generaladmission">FREE ENTRY- Book a ticket</a><br/>
+      From the 18th May:
+      @fa('ticket-alt', 'ticket', 'mr-2') <a class="free_ticket" href="https://tickets.museums.cam.ac.uk/overview/generaladmission">FREE ENTRY- Book a ticket</a><br/>
       Wednesday - Saturday: 10:00 - 17:00 |
       Sundays and Bank Holiday Mondays: 12:00 - 17:00<br />
       Closed 24th December - 1st January inclusive.
-      @fa('door-closed', 'closed', 'mr-2') <a class="free_ticket" href="{{ URL::to('/visit-us/gallery-closures-and-collection-updates') }}" >Gallery closures and collection updates</a> --}}
-      Museums and galleries in Cambridge will be closed to the public as part of a period of national/local
-      restrictions.
+      @fa('door-closed', 'closed', 'mr-2') <a class="free_ticket" href="{{ URL::to('/visit-us/gallery-closures-and-collection-updates') }}" >Gallery closures and collection updates</a>
+      {{-- Museums and galleries in Cambridge will be closed to the public as part of a period of national/local
+      restrictions. --}}
     </p>
   </div>
 </div>
