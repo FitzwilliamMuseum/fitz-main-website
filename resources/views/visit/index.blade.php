d82dd576c79fed27b2882a819ea4dc25f3aa99fd
Daniel Pett <dejp3@cam.ac.uk>
2021-05-11 07:21:32 +0100

    Add coordinates for entrances

--- resources/views/visit/index.blade.php
+++ resources/views/visit/index.blade.php
@@ -33,10 +33,31 @@
     'maxZoom' => 18,
     'markers' => [
         [
-            'title' => 'Find the museum',
+            'title' => 'The Founder\'s Entrance',
             'lat' => 52.200278,
             'lng' => 0.119444,
-            'popup' => '<h3>The Museum</h3><p>We are located here</p>',
+            'popup' => '<h3>Founder\'s Entrance</h3><p>Access is via a flight of steps.</p>',
+
+        ],
+        [
+            'title' => 'The Courtyard Entrance',
+            'lat' => 52.19974696477096,
+            'lng' => 0.12065154202527271,
+            'popup' => '<h3>Courtyard Entrance</h3><p>Accessible step free access.</p>',
+
+        ],
+        [
+            'title' => 'Cambridge Station',
+            'lat' => 52.19433858460627,
+            'lng' => 0.1374599593479488,
+            'popup' => '<h3>Cambridge Station</h3><p>The railway station.</p>',
+
+        ],
+        [
+            'title' => 'Cambridge Bus Station',
+            'lat' => 52.2054313453068,
+            'lng' => 0.12392614778108904,
+            'popup' => '<h3>Cambridge Bus Station</h3><p>The central bus station.</p>',
 
         ],
     ],
