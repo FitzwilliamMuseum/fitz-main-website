0e3700961c4b8da9ec4aa3f6e4b09fc7b7a00a29
Daniel Pett <dejp3@cam.ac.uk>
2021-05-23 20:15:07 +0100

    Fix conservation

--- app/Http/Controllers/departmentsController.php
+++ app/Http/Controllers/departmentsController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\DirectUs;
-use Solarium\Core\Client\Client;
+use Solarium\Client;
+use Solarium\Core\Client\Adapter\Curl;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Solarium\Exception;
 use Illuminate\Support\Facades\Cache;
 
@@ -106,7 +108,7 @@ class departmentsController extends Controller
         $data = Cache::store('file')->get($key);
     } else {
         $configSolr = \Config::get('solarium');
-        $client = new Client($configSolr);
+        $client = new Client(new Curl(), new EventDispatcher(), $configSolr);
         $query = $client->createSelect();
         $query->setQuery('contentType:conservationblog title:*');
         $query->setRows(3);
