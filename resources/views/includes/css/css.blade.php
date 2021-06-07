e17371dc4fb3cf75d170e642d1028880547a5c63
Daniel Pett <dejp3@cam.ac.uk>
2021-06-07 14:43:39 +0100

    Change css position

--- resources/views/includes/css/css.blade.php
+++ resources/views/includes/css/css.blade.php
@@ -15,7 +15,7 @@ integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeV
   background-image: url(@yield('hero_image'));
   background-repeat: no-repeat;
   background-size: cover;
-  background-position: center;
+  background-position: 50% 30%;
 }
 
 @hasSection('parallax_two')
