cf0abeafc4465569a94dc086a2b3c1c7d534301e
Daniel Pett <dejp3@cam.ac.uk>
2021-06-11 18:22:36 +0100

    Add lead css

--- resources/views/includes/structure/head.blade.php
+++ resources/views/includes/structure/head.blade.php
@@ -2,6 +2,6 @@
 </div>
 <div class="container-fluid bg-white text-black text-center">
   <div class="ml-3 pt-4 pb-4">
-    <h1 class="shout" id="doc-main-h1">@yield('title')</h1>
+    <h1 class="shout lead" id="doc-main-h1">@yield('title')</h1>
   </div>
 </div>
