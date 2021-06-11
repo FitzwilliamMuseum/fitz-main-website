4599522874b2029529610382cf48db7cf5ad2a94
Daniel Pett <dejp3@cam.ac.uk>
2021-06-11 20:52:40 +0100

    Text muted remove

--- resources/views/news/article.blade.php
+++ resources/views/news/article.blade.php
@@ -115,8 +115,8 @@
           <h3 class="lead">
             <a href="{{ route('article', $record['slug'][0]) }}">{{ $record['title'][0] }}</a>
           </h3>
-          <h4>
-            <small class="text-muted">{{  Carbon\Carbon::parse($record['pubDate'][0])->format('l j F Y') }}</small>
+          <h4 class="text-info lead">
+            {{  Carbon\Carbon::parse($record['pubDate'][0])->format('l j F Y') }}
           </h4>
         </div>
       </div>
