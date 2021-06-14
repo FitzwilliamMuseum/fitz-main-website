5c4e1404f7308742ce67c2017c1ea5314bbb379c
Daniel Pett <dejp3@cam.ac.uk>
2021-06-14 15:45:35 +0100

    Add email/phone

--- resources/views/includes/elements/book.blade.php
+++ resources/views/includes/elements/book.blade.php
@@ -10,11 +10,17 @@
           <span class="text-success">Booking essential</span>
         </h2>
         <a class="btn btn-dark btn-large" href="https://tickets.museums.cam.ac.uk/overview/generaladmission">Book free entry now</a>
-        <p class="text-info mt-3">Find us:
-            The Fitzwilliam Museum,
+        <h3 class="text-info mt-3 lead">Find us:</h3>
+        <p class="text-info mt-3">The Fitzwilliam Museum,
             Trumpington Street,
             Cambridge
-            CB2 1RB</p>
+            CB2 1RB
+            <br />
+            @fa('phone') +44 (0)1223 333 230
+            <br />
+            <a href="mailto:tickets@museums.cam.ac.uk">@fa('at') tickets@museums.cam.ac.uk</a>
+        </p>
+
       </div>
     </div>
     <div class="col-md-4">
