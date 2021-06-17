f3f3200941c15a79eb02d4c23c2059831ad5006f
Daniel Pett <dejp3@cam.ac.uk>
2021-06-17 17:58:54 +0100

    Change to 1by1

--- resources/views/includes/elements/iiif.blade.php
+++ resources/views/includes/elements/iiif.blade.php
@@ -17,7 +17,7 @@ foreach ($record['_source']['multimedia'] as $image ){
     @if(Arr::get($con, 'zoom', true))
       @if(array_key_exists('type', $id))
         @if($id['type'] === 'priref')
-          <div class="embed-responsive embed-responsive-16by9">
+          <div class="embed-responsive embed-responsive-1by1">
             <iframe src="https://collection.beta.fitz.ms/uv.html#?manifest=https://api.fitz.ms/data-distributor/iiif/object-{{ $id['value']}}/manifest&c=0&m=0&cv=0&config=https://collection.beta.fitz.ms/config.json&locales=en-GB:English (GB),cy-GB:Cymraeg,fr-FR:Français (FR),sv-SE:Svenska,xx-XX:English (GB) (xx-XX)&xywh=-2139,-341,9925,5028&r=0"
             class="embed-item-responsive" allowfullscreen frameborder="0"></iframe>
           </div>
