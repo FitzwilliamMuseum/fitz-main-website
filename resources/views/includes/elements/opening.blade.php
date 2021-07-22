413ef160d0ec52bd5ef4c0c132527817dbd69fef
Daniel Pett <dejp3@cam.ac.uk>
2021-07-22 11:14:47 +0100

    Change opening

--- resources/views/includes/elements/opening.blade.php
+++ resources/views/includes/elements/opening.blade.php
@@ -3,6 +3,6 @@
   @fa('ticket-alt', 'ticket', 'mr-2') <a class="free_ticket" href="https://tickets.museums.cam.ac.uk/overview/generaladmissionjuly">FREE ENTRY- Book a ticket</a><br/>
   Tuesday - Saturday: 10:00 - 17:00 |
   Sundays and Bank Holiday Mondays: 12:00 - 17:00<br />
-  Closed 24th December - 1st January inclusive.
+  Closed 24th December to 27th December 2021 and 31st December 2021 to 1st January 2022 inclusive
   @fa('door-closed', 'closed', 'mr-2') <a class="free_ticket" href="{{ URL::to('/visit-us/gallery-closures-and-collection-updates') }}" >Gallery closures and collection updates</a>
 </p>
