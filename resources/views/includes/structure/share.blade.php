71f82aff18253a8c595cd826579b9526573dd4b0
Daniel Pett <dejp3@cam.ac.uk>
2021-08-03 22:17:46 +0100

    Add insta

--- resources/views/includes/structure/share.blade.php
+++ resources/views/includes/structure/share.blade.php
@@ -6,6 +6,7 @@
         <ul class="text-center conversation-footer">
             <li><a aria-label="Share on Facebook" href="https://www.facebook.com/sharer/sharer.php?u={{ URL::current() }}"><i class="fab fa-facebook fa-2x p-2"></i></a></li>
             <li><a aria-label="Share on Twitter" href="https://twitter.com/intent/tweet?url={{ URL::current() }}%2F&text=@yield('title')"><i class="fab fa-twitter fa-2x p-2"></i></a></li>
+            <li><a aria-label="Tag us on Instagram" href="https://instagram.com/fitzmuseum_uk"><i class="fab fa-instagram fa-2x p-2"></i></a></li>
         </ul>
       </div>
     </div>
