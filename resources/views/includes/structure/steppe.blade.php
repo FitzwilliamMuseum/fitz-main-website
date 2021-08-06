e71e15666b711288c7bdd0be63cd40ad1dee4cd7
Daniel Pett <dejp3@cam.ac.uk>
2021-08-06 17:43:32 +0100

    Add sr-only

--- resources/views/includes/structure/steppe.blade.php
+++ resources/views/includes/structure/steppe.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid steppe align-text-bottom p-3">
+<div class="container-fluid steppe align-text-bottom p-3 sr-only">
   {{-- <div class="container align-bottom my-5">
     <span class="align-text-bottom"><h2 class="display-2 text-white">Gold of the Great Steppe</h2>
   </div> --}}
