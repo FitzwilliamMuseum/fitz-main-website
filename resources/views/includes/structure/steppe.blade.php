55303a6f0fbc96d487ab79e6364219e414ccb6cc
Daniel Pett <dejp3@cam.ac.uk>
2021-08-09 10:56:06 +0100

    Update banner

--- resources/views/includes/structure/steppe.blade.php
+++ resources/views/includes/structure/steppe.blade.php
@@ -4,6 +4,12 @@
         Gold of the Great Steppe
       </h2>
 </div>
+<div class="container-fluid bg-black">
+  <div class="text-center text-white p-3">
+    <p>Member priority booking open 10th August, general release 16th August</p>
+    <a class="btn btn-outline-light" href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=2">Become part of something special, join our friends now</a>
+  </div>
+</div>
 <style>
 .steppe {
   min-height: 600px;
