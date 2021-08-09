3c99a55e5878cb17b06fc00e72b23a6056b22243
Daniel Pett <dejp3@cam.ac.uk>
2021-08-09 08:15:27 +0100

    Add  style

--- resources/views/includes/structure/steppe.blade.php
+++ resources/views/includes/structure/steppe.blade.php
@@ -4,3 +4,13 @@
         Gold of the Great Steppe
       </h2>
 </div>
+<style>
+.steppe {
+  min-height: 600px;
+  background-image: url('https://fitz-cms-images.s3.eu-west-2.amazonaws.com/kazakh-compressed.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 50%;
+
+}
+</style>
