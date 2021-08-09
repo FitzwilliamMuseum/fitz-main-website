1120910f28cda251b2348d797f9a23d8a3139801
Daniel Pett <dejp3@cam.ac.uk>
2021-08-09 11:34:37 +0100

    Move steppe h2

--- resources/views/includes/structure/steppe.blade.php
+++ resources/views/includes/structure/steppe.blade.php
@@ -1,11 +1,11 @@
 <div class="container-fluid steppe p-3">
-    <span class="align-text-bottom text-center">
-      <h2 class="display-2 text-white">
-        Gold of the Great Steppe
-      </h2>
+
 </div>
 <div class="container-fluid bg-black">
   <div class="text-center text-white p-3">
+    <h2 class="display-3 text-white">
+      Gold of the Great Steppe
+    </h2>
     <p>Member priority booking open 10th August, general release 16th August</p>
     <a class="btn btn-outline-light" href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=2">Become part of something special, join our friends now</a>
   </div>
