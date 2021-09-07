fa952447efc7ca3c688528be0dc53ccd7f5deb6f
Daniel Pett <dejp3@cam.ac.uk>
2021-09-07 22:43:40 +0100

    Adjust feed

--- resources/views/vendor/feed/atom.blade.php
+++ resources/views/vendor/feed/atom.blade.php
@@ -1,45 +1,41 @@
 <?php
 
-    /* Using an echo tag here so the `<? ... ?>` won't get parsed as short tags */
-    '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
+/* Using an echo tag here so the `<? ... ?>` won't get parsed as short tags */
+'<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
 ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-    @foreach($meta as $key => $metaItem)
-        @if($key === 'link')
-            <{{ $key }} href="{{ url($metaItem) }}" rel="self"></{{ $key }}>
-        @elseif($key === 'title')
-            <{{ $key }}><![CDATA[{{ $metaItem }}]]></{{ $key }}>
-        @else
-            <{{ $key }}>{{ $metaItem }}</{{ $key }}>
-        @endif
-    @endforeach
+  <title>{{ $meta['link'] }}</title>
+  <link href="{{ $meta['link'] }}" rel="self"/>
+  <logo>{{ $meta['image']}}</logo>
+  <updated>{{ $meta['updated'] }}</updated>
+  <author>
+    <name>The Fitzwilliam Museum</name>
+    <email>press@fitzmuseum.cam.ac.uk</email>
+  </author>
+  <rights>Chancellor, Masters, and Scholars of the University of Cambridge: CC-BY</rights>
+  <generator>Fitzwilliam Museum Digital Magic by DEJP3</generator>
+  <id>{{ $meta['id'] }}</id>
+  @foreach($items as $item)
+    <entry>
+      <title><![CDATA[{{ $item->title }}]]></title>
+      <link rel="alternate" href="{{ url($item->link) }}" />
+      <id>{{ $item->id }}</id>
       <author>
-        <name>The Fitzwilliam Museum</name>
-        <email>press@fitzmuseum.cam.ac.uk</email>
+        <name><![CDATA[{{ $item->authorName }}]]></name>
       </author>
-      <rights>Chancellor, Masters, and Scholars of the University of Cambridge: CC-BY</rights>
-      <generator>Fitzwilliam Museum Digital Magic by DEJP3</generator>
-    @foreach($items as $item)
-        <entry>
-            <title><![CDATA[{{ $item->title }}]]></title>
-            <link rel="alternate" href="{{ url($item->link) }}" />
-            <id>{{ $item->id }}</id>
-            <author>
-                <name><![CDATA[{{ $item->authorName }}]]></name>
-            </author>
-            <summary type="html">
-                <![CDATA[{!! $item->summary !!}]]>
-            </summary>
-            <content type="html">
-              <![CDATA[{!! $item->content !!}]]>
-            </content>
-            @if($item->__isset('enclosure'))
-              <enclosure url="{{ url($item->enclosure) }}" length="{{ $item->enclosureLength }}" type="{{ $item->enclosureType }}" />
-            @endif
-            {{-- <category type="html">
-                <![CDATA[{!! $item->category ?? '' !!}]]>
-            </category> --}}
-            <updated>{{  date(DATE_ATOM, strtotime($item->updated ))}}</updated>
-        </entry>
-    @endforeach
+      <summary type="html">
+        <![CDATA[{!! $item->summary !!}]]>
+      </summary>
+      <content type="html">
+        <![CDATA[{!! $item->content !!}]]>
+      </content>
+      @if($item->__isset('enclosure'))
+        <enclosure url="{{ url($item->enclosure) }}" length="{{ $item->enclosureLength }}" type="{{ $item->enclosureType }}" />
+        @endif
+        {{-- <category type="html">
+        <![CDATA[{!! $item->category ?? '' !!}]]>
+      </category> --}}
+      <updated>{{  date(DATE_ATOM, strtotime($item->updated ))}}</updated>
+    </entry>
+  @endforeach
 </feed>
