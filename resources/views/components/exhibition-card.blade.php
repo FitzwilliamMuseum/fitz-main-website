0098a318de0b83bd33774a58dc9cabb6a92fdf07
Daniel Pett <dejp3@cam.ac.uk>
2021-10-04 00:23:57 +0100

    Add test

--- resources/views/components/exhibition-card.blade.php
+++ resources/views/components/exhibition-card.blade.php
@@ -21,7 +21,7 @@
             {{ $title }}
           </a>
         </h3>
-        @if($status === 'current' && $ticketed === true)
+        @if($status === 'current' && $ticketed === true && !is_null($tessitura_string))
           <p class="text-info">Ticket and timed entry</p>
           <a class="btn btn-dark mb-2" href="https://tickets.museums.cam.ac.uk/overview/{{ $tessitura_string }}">Book now</a>
         @elseif($status === 'current')
