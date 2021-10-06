157b648ac64741095a0a4ef676537410d3c734d6
Daniel Pett <dejp3@cam.ac.uk>
2021-10-07 00:15:01 +0100

    Change steppe layout

--- resources/views/includes/structure/steppe.blade.php
+++ resources/views/includes/structure/steppe.blade.php
@@ -12,11 +12,12 @@
 </div>
 <style>
 .steppe {
-  min-height: 600px;
-  background-image: url('https://fitz-cms-images.s3.eu-west-2.amazonaws.com/kazakh-compressed.jpg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 50% 50%;
+  min-height: 400px;
+  background-color: black;
+  background-image: url('https://fitz-cms-images.s3.eu-west-2.amazonaws.com/kazakh-compressed-smaller.jpg');
+  background-repeat:no-repeat;
+background-size:contain;
+background-position:center;
 }
 .steppe-link {
   border-bottom: 2px solid currentColor;
