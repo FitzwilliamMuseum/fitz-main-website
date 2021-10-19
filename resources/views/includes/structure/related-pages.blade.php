939533eb1085e4e9b6e9e342737e9173997f2e55
Daniel Pett <dejp3@cam.ac.uk>
2021-10-19 19:31:13 +0100

    Change img size

--- resources/views/includes/structure/related-pages.blade.php
+++ resources/views/includes/structure/related-pages.blade.php
@@ -10,7 +10,7 @@
       <div class="col-md-4 mb-3">
         <div class="card  h-100">
           @if(!is_null($record['thumbnail']))
-            <a href="{{ $record['url'][0]}}"><img class="img-fluid" src="{{ $record['thumbnail'][0]}}"
+            <a href="{{ $record['url'][0]}}"><img class="img-fluid" src="{{ $record['smallimage'][0]}}"
               alt="Highlight image for {{ $record['title'][0] }}" /></a>
             @else
               <a href="{{ $record['url'][0]}}"><img class="img-fluid" src="https://content.fitz.ms/fitz-website/assets/gallery3_roof.jpg?key=directus-large-crop"
