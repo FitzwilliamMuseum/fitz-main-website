3be1ea080fc74ebcecb600930517a8e4aedaa5b3
Daniel Pett <dejp3@cam.ac.uk>
2021-10-19 12:53:08 +0100

    Change wording

--- resources/views/includes/elements/book.blade.php
+++ resources/views/includes/elements/book.blade.php
@@ -9,13 +9,13 @@
             <span class="text-white">Booking essential</span>
           </h2>
           <a class="btn btn-info btn-large" href="https://tickets.museums.cam.ac.uk/overview/generaladmission">Book free entry now</a>
-          <h3 class="text-white mt-3 lead">Find us:</h3>
-          <p class="text-white mt-3">The Fitzwilliam Museum,
-              Trumpington Street,
-              Cambridge
-              CB2 1RB
+          <h3 class="text-white mt-3 lead">Booking and ticketing assistance</h3>
+          <p class="text-white mt-3">
+            For assistance with booking, or for ticket enquiries, please contact us.
               <br />
-              @fa('phone') +44 (0)1223 333 230
+              Box office open 10:00 - 17:00 Tuesday - Saturday, 12:00 - 17:00 Sunday. Closed Monday.
+              <br/>
+              @fa('phone') +44 (0)1223 333 230 @fa('envelope') <a href="tickets@museums.cam.ac.uk" class="text-white">tickets@museums.cam.ac.uk</a>
           </p>
         </div>
       </div>
