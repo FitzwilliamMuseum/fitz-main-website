b4136ce64a1cd2c96c74bceecee6eb247b7e0192
Daniel Pett <dejp3@cam.ac.uk>
2021-11-04 10:14:05 +0000

    Add mix to the system for css

--- resources/views/includes/css/css.blade.php
+++ resources/views/includes/css/css.blade.php
@@ -5,8 +5,9 @@
     integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ mix('css/fitzwilliam.css') }}" />
 
-    <link href="{{ URL::to('/css/site.css') }}" rel="stylesheet" media="screen">
+    {{-- <link href="{{ URL::to('/css/site.css') }}" rel="stylesheet" media="screen"> --}}
     @hasSection('audio-guide')
       <link href="https://cdnjs.cloudflare.com/ajax/libs/plyr/3.5.6/plyr.css" rel="stylesheet" media="screen">
     @endif
