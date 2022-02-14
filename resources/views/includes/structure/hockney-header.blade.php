1760c11b4a78e342f578d099c775c65c496d2d67
Daniel Pett <dejp3@cam.ac.uk>
2022-02-14 09:28:41 +0000

    Add hockney materials with links

--- resources/views/includes/structure/hockney-header.blade.php
+++ resources/views/includes/structure/hockney-header.blade.php
@@ -2,16 +2,16 @@
     <div class="container-fluid mt-5 hockney_header">
         <div class="row d-flex h-100 hockney_sub">
             <div class="col-md-6 px-0">
-                <img
+                <a href="{{ route('exhibition',['hockneys-eye-the-art-and-technology-of-depiction']) }}" title="Exhibition details"><img
                     src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/david-hockney_le-pac-des-sources-vichy_70a36-resize-1-.jpg"
                     class="img-fluid"
                     alt="Le Parc des Sources, David Hockney 1970"
-                />
+                    /></a>
             </div>
-            <div class="col-md-6 justify-content-center align-self-center">
-                <p class="text-center text-white py-3 hockney-title">Hockney's Eye</p>
-                <p class="text-center text-white hockney-subtitle">The Art and Technology of Depiction</p>
-                <p class="text-center text-white py-1 hockney-dates">15 March – 29 August 2022</p>
+            <div class="col-md-6 justify-content-center align-self-center hockney__link">
+                <a href="{{ route('exhibition',['hockneys-eye-the-art-and-technology-of-depiction']) }}" title="Exhibition details"><p class="text-center text-white py-3 hockney-title">Hockney's Eye</p>
+                    <p class="text-center text-white hockney-subtitle">The Art and Technology of Depiction</p>
+                    <p class="text-center text-white py-1 hockney-dates">15 March – 29 August 2022</p></a>
             </div>
 
         </div>
