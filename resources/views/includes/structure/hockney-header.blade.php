42e02ba2a98a9da59dab4d4df514c831dc902825
Daniel Pett <dejp3@cam.ac.uk>
2022-02-24 23:05:26 +0000

    Fix image that was removed

--- resources/views/includes/structure/hockney-header.blade.php
+++ resources/views/includes/structure/hockney-header.blade.php
@@ -4,19 +4,19 @@
             <div class="col-md-5 px-0">
                 <a href="{{ route('exhibition',['hockneys-eye-the-art-and-technology-of-depiction']) }}"
                    title="Exhibition details"><img
-                        src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/hockney.jpg"
+                        src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/david-hockney_21a14-self-portrait-1-.jpg"
                         class="img-fluid"
                         alt="Self portrait of David Hockney"
                     /></a>
             </div>
-            <div class="col-md-7 hockney__link">
+               <div class="col-md-7 hockney__link">
                 <a href="{{ route('exhibition',['hockneys-eye-the-art-and-technology-of-depiction']) }}"
                    title="Exhibition details">
                     <p class="text-center sr-only text-white py-3 hockney-title">Hockney's Eye</p>
                     <p class="sr-only text-center text-white hockney-subtitle">The Art and Technology of Depiction</p>
-                    <p class="sr-only text-center text-white py-1 hockney-dates">15 March – 29 August 2022</p></a>
+                    <p class="sr-only text-center text-white py-1 hockney-dates">15 March – 29 August 2022</p>
+                  </a>
             </div>
-
         </div>
     </div>
 </div>
