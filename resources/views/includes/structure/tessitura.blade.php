d32cf0abbba66eaa65eed965e7688b8673d0c18c
Daniel Pett <dejp3@cam.ac.uk>
2022-03-15 10:59:16 +0000

    Change colour

--- resources/views/includes/structure/tessitura.blade.php
+++ resources/views/includes/structure/tessitura.blade.php
@@ -1,6 +1,6 @@
 <div class="col-12 shadow-sm p-3 mx-auto mb-3">
     <div class="text-center">
-        <a class="btn btn-lg btn-dark d-block" title="Book tickets for this exhibition"
+        <a class="btn btn-lg btn-hockney d-block" title="Book tickets for this exhibition"
            href="https://tickets.museums.cam.ac.uk/overview/{{ $coll['tessitura_string'] }}">Book your FREE tickets
             for {{ $coll['exhibition_title'] }}</a>
     </div>
