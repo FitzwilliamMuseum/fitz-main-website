7c691f691ed03ccc23d90e4552c4b76dab981b96
Daniel Pett <dejp3@cam.ac.uk>
2022-03-21 11:03:16 +0000

    Fix sessions debug

--- resources/views/components/exhibition-thanks.blade.php
+++ resources/views/components/exhibition-thanks.blade.php
@@ -1,6 +1,8 @@
-<div class="container">
-    <div class="col-12 shadow-sm p-3 mx-auto mb-3 mt-3">
-        <h3>Acknowledgements</h3>
-        @markdown($exhibition['exhibition_thank_you'])
+@if(!empty($exhibition['exhibition_thank_you']))
+    <div class="container">
+        <div class="col-12 shadow-sm p-3 mx-auto mb-3 mt-3">
+            <h3>Acknowledgements</h3>
+            @markdown($exhibition['exhibition_thank_you'])
+        </div>
     </div>
-</div>
+@endif
--- resources/views/includes/structure/families.blade.php
+++ resources/views/includes/structure/families.blade.php
@@ -1,5 +1,4 @@
 <div class="row">
-    @dd($sessions)
     @foreach($sessions['data'] as $session)
         <x-family-video :session="$session"></x-family-video>
     @endforeach
