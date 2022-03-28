6b0b6f9e781fabd1599fd847892a0640441326d7
Daniel Pett <dejp3@cam.ac.uk>
2022-03-28 11:37:00 +0100

    Remove extra id string

--- resources/views/includes/elements/identification.blade.php
+++ resources/views/includes/elements/identification.blade.php
@@ -2,9 +2,7 @@
 <ul>
   @foreach($record['_source']['identifier'] as $id)
     @if(array_key_exists('type', $id))
-      @if($id['type'] === 'uri')
-        <li class="sr-only"><a href="{{ env('COLLECTION_URL') }}/id/object/{{ $id['value']}}">Stable URI</a></li>
-      @elseif($id['type'] === 'priref')
+      @if($id['type'] === 'priref')
         <li>Primary reference Number: <a href="{{ env('COLLECTION_URL') }}/id/object/{{ $id['value']}}">{{ $id['value']}}</a></li>
       @elseif($id['type'] === 'Online 3D model')
         <li class="sr-only"><a href="https://sketchfab.com/3d-models/{{ $id['value']}}">Sketchfab model</a></li>
