87505ab8a8b103752858c4e28b48e7f3ccf64a5c
Daniel Pett <dejp3@cam.ac.uk>
2022-03-28 07:58:02 +0100

    Update path for products

--- resources/views/components/shopify-live-card.blade.php
+++ resources/views/components/shopify-live-card.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-4 mb-3">
     <div class="card card-fitz h-100">
-        <a href="{{ env('SHOPIFY_FME_PROTOCOL') }}{{ env('SHOPIFY_FME_URL') }}{{ env('SHOPIFY_FME_CATALOGUE') }}{{ $result['handle'] }}">
+        <a href="{{ env('SHOPIFY_FME_PROTOCOL') }}{{ env('SHOPIFY_FME_LIVE_URL') }}{{ env('SHOPIFY_FME_CATALOGUE') }}{{ $result['handle'] }}">
             <img class="card-img-top" src="{{ $result['image']['src']}}"
                  alt="A product image depicting {{ $result['image']['src'] }}"
                  loading="lazy"/>
@@ -9,7 +9,7 @@
         <div class="card-body h-100">
             <div class="contents-label mb-3">
                 <h3>
-                    <a href="{{ env('SHOPIFY_FME_PROTOCOL') }}{{ env('SHOPIFY_FME_URL') }}{{ env('SHOPIFY_FME_CATALOGUE') }}{{ $result['handle'] }}" class="stretched-link">
+                    <a href="{{ env('SHOPIFY_FME_PROTOCOL') }}{{ env('SHOPIFY_FME_LIVE_URL') }}{{ env('SHOPIFY_FME_CATALOGUE') }}{{ $result['handle'] }}" class="stretched-link">
                         {{ $result['title'] }}
                     </a>
                 </h3>
