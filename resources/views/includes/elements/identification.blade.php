3dc71ff252e5d1d94fac9f1c14619b8399ba92b6
Daniel Pett <dejp3@cam.ac.uk>
2022-03-28 10:25:23 +0100

    Fix http to https url for screen reader link

--- resources/views/includes/elements/identification.blade.php
+++ resources/views/includes/elements/identification.blade.php
@@ -3,7 +3,7 @@
   @foreach($record['_source']['identifier'] as $id)
     @if(array_key_exists('type', $id))
       @if($id['type'] === 'uri')
-        <li class="sr-only"><a href="{{ $id['value']}}">Stable URI</a></li>
+        <li class="sr-only"><a href="{{ env('COLLECTION_URL') }}/id/object/{{ $id['value']}}">Stable URI</a></li>
       @elseif($id['type'] === 'priref')
         <li>Primary reference Number: <a href="{{ env('COLLECTION_URL') }}/id/object/{{ $id['value']}}">{{ $id['value']}}</a></li>
       @elseif($id['type'] === 'Online 3D model')
