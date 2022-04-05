f7a882cf5c00be367306b1e299fcb08034a67c88
Daniel Pett <dejp3@cam.ac.uk>
2022-04-06 00:17:11 +0100

    Add more meta

--- resources/views/components/ttn-artist-detail.blade.php
+++ resources/views/components/ttn-artist-detail.blade.php
@@ -1,6 +1,8 @@
 <div class="container-fluid py-3">
     <div class="container">
         <div class=" py-3 bg-white">
+            <p>Nationality: {{$artist[0]['nationality'] ?? 'Not known'}}</p>
+            <p>{{$artist[0]['year_of_birth']}} - {{$artist[0]['year_of_death']}}</p>
             @markdown($artist[0]['biography'])
         </div>
     </div>
