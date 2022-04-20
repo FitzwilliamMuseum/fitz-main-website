1e0c15331e83adab4c67b1b5bce91d38cd0969fe
Daniel Pett <dejp3@cam.ac.uk>
2022-04-20 18:07:46 +0100

    Adjust buttons

--- resources/views/includes/structure/tessitura.blade.php
+++ resources/views/includes/structure/tessitura.blade.php
@@ -1,6 +1,12 @@
 <div class="col-12 shadow-sm p-3 mx-auto mb-3">
     <div class="text-center">
-        <a class="btn btn-lg btn-hockney w-50" title="Book tickets for this exhibition"
-           href="https://tickets.museums.cam.ac.uk/overview/{{ $coll['tessitura_string'] }}">Book tickets</a>
+        <a title="Book tickets for this exhibition"
+           href="https://tickets.museums.cam.ac.uk/overview/{{ $coll['tessitura_string'] }}"
+           @isset($coll['button_class'])
+           class="btn btn-lg btn-{{$coll['button_class']}} w-50"
+           @else
+           class="btn btn-lg btn-dark w-50"
+            @endif
+        >Book tickets</a>
     </div>
 </div>
