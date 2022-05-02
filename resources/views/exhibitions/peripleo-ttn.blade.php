9846287f877b2e3b7e77729f15961dc988db9de3
Daniel Pett <dejp3@cam.ac.uk>
2022-05-02 23:57:20 +0100

    Allow fullscreen

--- resources/views/exhibitions/peripleo-ttn.blade.php
+++ resources/views/exhibitions/peripleo-ttn.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.peripleo')
 @section('content')
-    <iframe src="https://peripleo.fitzmuseum.cam.ac.uk/#/4.94/1.4069/50.4439/mode=points" title="A Peripleo map" />
+    <iframe
+        src="https://peripleo.fitzmuseum.cam.ac.uk/#/4.94/1.4069/50.4439/mode=points"
+        title="A Peripleo map"
+        allowfullscreen
+        width="100%"
+        height="100%"
+    />
 @endsection
