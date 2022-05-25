46c233630cd0c6978ff5d2a63f6bb874517d2922
Daniel Pett <dejp3@cam.ac.uk>
2022-05-25 12:33:18 +0100

    Change dims

--- resources/views/components/image-card.blade.php
+++ resources/views/components/image-card.blade.php
@@ -6,7 +6,6 @@
                      src="{{ $image['data']['thumbnails'][13]['url']}}"
                      alt="{{ $altTag }}"
                      width="{{ $image['data']['thumbnails'][13]['width']}}"
-                     height="{{ $image['data']['thumbnails'][13]['height']}}"
                      loading="lazy"
                 />
             </a>
