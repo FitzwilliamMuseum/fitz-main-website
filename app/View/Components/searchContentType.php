bd0d0462410c6e65cedf701bef861fe28d0dbd84
Daniel Pett <dejp3@cam.ac.uk>
2022-06-05 00:43:18 +0100

    Add defaults

--- app/View/Components/searchContentType.php
+++ app/View/Components/searchContentType.php
@@ -20,7 +20,7 @@ class searchContentType extends Component
     public function __construct(string $type, string $display_name = NULL)
     {
         $this->type = $type;
-        if(is_null($display_name)) {
+        if($display_name != '') {
             $this->display_name = $this->getDisplayName($type);
         } else {
             $this->display_name = $type;
@@ -29,7 +29,7 @@ class searchContentType extends Component
 
     public function getDisplayName(string $type): string
     {
-        return SearchContentTypes::find($type)['display_name'] ?? 'No type found';
+        return SearchContentTypes::find($type)['display_name'];
     }
 
     /**
