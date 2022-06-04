14eafd4a8530ae9558c569acb57fbc31f3afc331
Daniel Pett <dejp3@cam.ac.uk>
2022-06-04 23:55:10 +0100

    Add method

--- app/View/Components/searchContentType.php
+++ app/View/Components/searchContentType.php
@@ -9,21 +9,27 @@ class searchContentType extends Component
 {
     public string $type;
     public string $displayName;
+
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct( string $type)
+    public function __construct(string $type)
     {
         $this->type = $type;
-        $this->displayName = SearchContentTypes::find($this->type)['display_name'] ?? 'No type found';
+        $this->displayName = $this->getDisplayName($type);
+    }
+
+    public function getDisplayName(string $type): string
+    {
+        return SearchContentTypes::find($type)['display_name'] ?? 'No type found';
     }
 
     /**
      * Get the view / contents that represent the component.
      *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
+     * @return View
      */
     public function render()
     {
