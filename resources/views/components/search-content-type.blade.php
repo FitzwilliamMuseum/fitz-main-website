c44ba6fae8ba7608113c6ad9d0c3d4f6c91d183a
Daniel Pett <dejp3@cam.ac.uk>
2022-06-05 00:01:24 +0100

    Remove dd

--- resources/views/components/search-content-type.blade.php
+++ resources/views/components/search-content-type.blade.php
@@ -1,4 +1,3 @@
 <span class="badge bg-info p-2 shorten-words text-truncate my-1 mr-2">
-    {{ dd(get_defined_vars()) }}
-    {{ $displayName }}
+    {{ $displayName ?? 'No type declared' }}
 </span>
