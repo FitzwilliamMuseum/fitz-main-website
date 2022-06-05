61aa5a0929b3be71f83a150dddfb7c1238c60a7a
Daniel Pett <dejp3@cam.ac.uk>
2022-06-05 08:16:30 +0100

    Add component

---
+++ app/View/Components/contentType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+use App\Models\SearchContentTypes;
+
+class contentType extends Component
+{
+    public string $type;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param $type
+     */
+    public function __construct($type)
+    {
+        $this->type = $this->getDisplayName($type);
+    }
+
+    public function getDisplayName(string $type): string
+    {
+        return SearchContentTypes::find($type)['display_name'];
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return View
+     */
+    public function render(): View
+    {
+        return view('components.search-content-type');
+    }
+}
