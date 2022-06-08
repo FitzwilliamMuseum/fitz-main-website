45025f5a3e97cc20d6608fb54a319279872a4552
Daniel Pett <dejp3@cam.ac.uk>
2022-06-08 10:04:36 +0100

    Fix viewpoint error

--- resources/views/components/viewpoint.blade.php
+++ resources/views/components/viewpoint.blade.php
@@ -1,22 +1,22 @@
 @section('title', $viewpoint['title'])
-@section('description', 'A viewpoint focused on ' . $image['ttn_labels_id']['alt_text'])
 <div>
     @foreach($viewpoint['associated_artworks'] as $image)
-        <figure class="figure">
-            <img class="figure-img img-fluid  my-2"
-                 src="{{$image['ttn_labels_id']['image']['data']['url']}}"
-                 alt="{{ $image['ttn_labels_id']['alt_text'] }}"
-                 width="{{ $image['ttn_labels_id']['image']['width'] }}"
-                 height="{{ $image['ttn_labels_id']['image']['height'] }}"
-                 loading="lazy"/>
-            <figcaption class="figure-caption text-info">
-                {{$image['ttn_labels_id']['image']['title']}}
-            </figcaption>
-        </figure>
+        @section('description', 'A viewpoint focused on ' . $image['ttn_labels_id']['alt_text'])
+    <figure class="figure">
+        <img class="figure-img img-fluid  my-2"
+             src="{{$image['ttn_labels_id']['image']['data']['url']}}"
+             alt="{{ $image['ttn_labels_id']['alt_text'] }}"
+             width="{{ $image['ttn_labels_id']['image']['width'] }}"
+             height="{{ $image['ttn_labels_id']['image']['height'] }}"
+             loading="lazy"/>
+        <figcaption class="figure-caption text-info">
+            {{$image['ttn_labels_id']['image']['title']}}
+        </figcaption>
+    </figure>
     @endforeach
 </div>
 
-<div class="py-3" >
+<div class="py-3">
     <blockquote class="blockquote">
         @markdown($viewpoint['viewpoint'])
         @if(!empty($viewpoint['poetry']))
@@ -25,7 +25,8 @@
     </blockquote>
     <h2 class="text-info">
         @foreach($viewpoint['associated_people'] as $person)
-            {{$person['associated_people_id']['display_name']}}@isset($person['associated_people_id']['associated_role']), {{$person['associated_people_id']['associated_role']}}
+            {{$person['associated_people_id']['display_name']}}@isset($person['associated_people_id']['associated_role'])
+                , {{$person['associated_people_id']['associated_role']}}
             @endisset
             @if(!empty($person['associated_people_id']['associated_institution']))
                 <br/>
