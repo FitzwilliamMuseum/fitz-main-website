7a190c1c58a2f375c6c322eaad4a4d8a984b0884
Daniel Pett <dejp3@cam.ac.uk>
2022-08-22 10:39:23 +0100

    Update Directus class Now uses construct method rather than getter and setter methods. This cuts down on verbosity of classes. #475

--- app/DirectUs.php
+++ app/DirectUs.php
@@ -10,96 +10,12 @@ class DirectUs
 {
 
     /**
-     * @var string
+     * @param string $_endpoint
+     * @param array $_args
+     * @param string|null $_fields
      */
-    public string $_directUsUrl = 'https://content.fitz.ms/fitz-website/items/';
-
-    /**
-     * @var string
-     */
-    public string $_endpoint = '';
-    /**
-     * @var array
-     */
-    public array $_args = array();
-
-    /**
-     * @var string
-     */
-    protected string $_meta = 'meta=total_count,result_count';
-    /**
-     * @var string
-     */
-    protected string $_fields = '*.*.*';
-
-    /**
-     * @return string
-     */
-    public function getFields(): string
+    public function __construct(public string $_endpoint, public array $_args, public ?string $_fields = null)
     {
-        return $this->_fields;
-    }
-
-    /**
-     * @param string $fields
-     * @return string
-     */
-    public function setFields(string $fields): string
-    {
-        $this->_fields = $fields;
-        return $this->_fields;
-    }
-
-    /**
-     * @return string
-     */
-    public function getMeta(): string
-    {
-        return $this->_meta;
-    }
-
-    /**
-     * @param array $args
-     * @return $this
-     */
-    public function setArguments(array $args): static
-    {
-        $this->_args = $args;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    #[Pure] public function getCallUrl(): string
-    {
-        return $this->getDirectUsUrl() . $this->getEndPoint() . $this->buildQuery();
-    }
-
-    /**
-     * @return string
-     */
-    public function getDirectUsUrl(): string
-    {
-        return $this->_directUsUrl;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEndPoint(): string
-    {
-        return $this->_endpoint;
-    }
-
-    /**
-     * @param $endpoint
-     * @return string
-     */
-    public function setEndpoint($endpoint): string
-    {
-        $this->_endpoint = $endpoint . '?';
-        return $this->_endpoint;
     }
 
     /**
@@ -107,15 +23,7 @@ class DirectUs
      */
     #[Pure] public function buildQuery(): string
     {
-        return http_build_query($this->getArgs());
-    }
-
-    /**
-     * @return array
-     */
-    public function getArgs(): array
-    {
-        return $this->_args;
+        return '?' . http_build_query($this->_args);
     }
 
     /**
@@ -123,7 +31,7 @@ class DirectUs
      */
     public function getData(): array
     {
-        $url = $this->getDirectUsUrl() . $this->getEndPoint() . $this->buildQuery();
+        $url = env('DIRECTUS_URL') . $this->_endpoint . $this->buildQuery();
         $key = md5($url);
         $expiresAt = now()->addMinutes(20);
         if (Cache::has($key)) {
