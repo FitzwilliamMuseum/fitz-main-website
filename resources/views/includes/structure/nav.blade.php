43f150955d1d4d9bf76e65f269986c6d8b4017dd
Sean Dunwoody <sean@studio24.net>
2022-09-08 19:50:32 +0100

    Add a temporary modal to the site advising the public that the museum is closed until the 10th September out of respect for the late Queen Elizabeth II

--- resources/views/includes/structure/nav.blade.php
+++ resources/views/includes/structure/nav.blade.php
@@ -1,3 +1,27 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" id="launchEmergencyModalButton" aria-hidden="true" tabindex="-1" style="border: 0; height: 0px; overflow: hidden; padding: 0; width: 0px;">
+  Launch static backdrop modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true" style="background: rgba(0, 0, 0, 0.25);">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Closed until Saturday 10 September</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      The Fitzwilliam is deeply saddened to hear of the death of Her Majesty The Queen. We are closed until Saturday 10 September as a mark of respect.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <!-- Nav bars -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-black fixed-top">
     <div class="container-fluid">
