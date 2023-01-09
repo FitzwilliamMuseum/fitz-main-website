efee3567d3b94e6aa3f72cafaf8e384ccb0a6120
Alan Isaacson <38862429+AlanJIsaacson@users.noreply.github.com>
2023-01-09 11:46:06 +0000

    Adjust right hand image to not be background image

--- resources/views/components/home-page-banner.blade.php
+++ resources/views/components/home-page-banner.blade.php
@@ -8,16 +8,18 @@
                         <img src="{{$banners['left_image']['data']['full_url']}}"
                              class="img-fluid"
                              alt="{{ $banners['left_image_alt_text'] }}"
-                             height="1200"
+                             height="1000"
                              width="1000"
                         />
                     </a>
                 </div>
 
-                <div class="col-md-6 exhibitions__link">
+                <div class="col-md-6 px-0 exhibitions__link">
                     <a href="{{ route('exhibition', $banners['linked_exhibition'][0]['exhibitions_id']['slug']   ) }}"
                        title="Exhibition details for {{ $banners['linked_exhibition'][0]['exhibitions_id']['exhibition_title'] }}"
-                       class="exhibitions__link__href"> </a>
+                       class="exhibitions__link__href">
+                        <img src="{{ $banners['right_image']['data']['full_url'] }}" class="img-fluid" alt="" height="1000" width="1000" />
+                    </a>
                     <p class="text-center visually-hidden text-white py-3 exhibitions-title">{{$banners['exhibition_title']}}</p>
                     @if(isset($data['exhibition_subtitle']))
                         <p class="visually-hidden text-center text-white exhibitions-subtitle">{{$banners['exhibition_subtitle']}}</p>
@@ -83,16 +85,19 @@
             text-decoration: none;
             color: white;
         }
+        
+        /*
         .exhibitions__link {
             min-height: 400px;
             background: url('{{ $banners['right_image']['data']['full_url'] }}') no-repeat center;
             background-size: contain;
         }
+        */
 
         .exhibitions__link__href {
             display: block;
             height: 100%;
-            width:100%;
+            width: 100%;
         }
         .btn-exhibitions{color:#fff;background-color:{{ $banners['button_colour'] }};border-color:{{ $banners['button_colour'] }}}
         .btn-exhibitions:hover{color:#fff;background-color:{{ $banners['button_colour'] }};border-color:{{ $banners['button_colour'] }}}
