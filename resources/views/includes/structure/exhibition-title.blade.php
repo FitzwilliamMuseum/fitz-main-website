153931846e6c815555cd05b5ef83d9027d56d306
Alan Isaacson <38862429+AlanJIsaacson@users.noreply.github.com>
2023-01-11 13:06:38 +0000

    Remove test class

--- resources/views/includes/structure/exhibition-title.blade.php
+++ resources/views/includes/structure/exhibition-title.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid bg-white text-black text-center">
     <div class="py-2">
-        <h1 class="shout lead testing" id="doc-main-h1">@yield('title')</h1>
+        <h1 class="shout lead" id="doc-main-h1">@yield('title')</h1>
     </div>
 </div>
