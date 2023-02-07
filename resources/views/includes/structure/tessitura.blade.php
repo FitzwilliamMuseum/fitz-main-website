fc4f43ee9e50b527ed006c640c3a4d8308d39a42
Sean Dunwoody <sean@studio24.net>
2023-02-07 09:01:04 +0000

    Remove the link title as the link text is sufficient

--- resources/views/includes/structure/tessitura.blade.php
+++ resources/views/includes/structure/tessitura.blade.php
@@ -1,7 +1,6 @@
 <div class="col-12 shadow-sm p-3 mx-auto mb-3">
     <div class="text-center">
-        <a title="Book free tickets for this exhibition"
-           href="https://tickets.museums.cam.ac.uk/overview/{{ $exhibition['tessitura_string'] }}"
+        <a href="https://tickets.museums.cam.ac.uk/overview/{{ $exhibition['tessitura_string'] }}"
            @isset($exhibition['button_class'])
                class="btn btn-lg btn-{{$exhibition['button_class']}} w-50"
            @else
