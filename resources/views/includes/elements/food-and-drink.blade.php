f20ed25cb2ece73157b4abdcb632c96f33c40c50
JamieC24 <jamie@studio24.net>
2023-03-29 15:37:11 +0100

    Added data/ folder to gitignore
    Added food-and-drink banner to shopping-and-eating page

---
+++ resources/views/includes/elements/food-and-drink.blade.php
@@ -0,0 +1,22 @@
+<div class="card bg-maroon mb-3 col-md-12">
+    <div class="row no-gutters">
+        <div class="col-md-8">
+            <div class="card-body">
+                <h2 class="lead display-4">
+                    <span class="text-white">Islanders Specials</span>
+                </h2>
+                <p class="text-white mt-3">
+                    To celebrate the Islanders exhibition, the Courtyard Kitchen is serving 2 Mediterranean Specials in addition to the lunch menu. Ticket holders can receive 10% off these specials.
+                </p>
+                <a class="text-center btn btn-outline-light" href="https://www.fitzmuseum.cam.ac.uk/visit-us/exhibitions/islanders">
+                    View the Islanders Exhibition Information
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <img
+                src="https://content.fitz.ms/fitz-website/assets/islanders-promo-image-for-banner-738x738.jpg?key=directus-medium-crop"
+                class="card-img-top" width="369" height="369" alt="Human touch">
+        </div>
+    </div>
+</div>
