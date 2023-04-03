9ab3adc3e54056990ffdab7aa52cc2f774476c51
Miro Ostafiński <ostafinskim@gmail.com>
2023-04-03 11:26:48 +0100

    margin & font-size

--- resources/views/includes/elements/payment-options.blade.php
+++ resources/views/includes/elements/payment-options.blade.php
@@ -29,13 +29,13 @@
             <h3 class="lead" style="font-weight: 500;">Friends individual</h3>
             <hr /> 
             <div class="col-md-4 d-flex border rounded text-center" style="flex-direction: column-reverse; padding: 40px; color:#299942;">
-                <span class="display-6">Friends individual</span>
+                <span class="lead">Friends individual</span>
                 <figure class="fluid d-flex">
                     <img class="img-fluid" style="max-width: 30%; margin: 0 auto; filter: invert(47%) sepia(44%) saturate(738%) hue-rotate(82deg) brightness(93%) contrast(89%);" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIiA+CiAgPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBkPSJNNy41IDZhNC41IDQuNSAwIDExOSAwIDQuNSA0LjUgMCAwMS05IDB6TTMuNzUxIDIwLjEwNWE4LjI1IDguMjUgMCAwMTE2LjQ5OCAwIC43NS43NSAwIDAxLS40MzcuNjk1QTE4LjY4MyAxOC42ODMgMCAwMTEyIDIyLjVjLTIuNzg2IDAtNS40MzMtLjYwOC03LjgxMi0xLjdhLjc1Ljc1IDAgMDEtLjQzNy0uNjk1eiIgY2xpcC1ydWxlPSJldmVub2RkIiAvPgo8L3N2Zz4KCg==" alt="user icon" />  
                 </figure>
   
             </div>
-            <div class="col-md-8">
+            <div class="col-md-8 mt-4">
                 <div class="card h-100 justify-content-center">
                     <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=2" class="align-self-center w-50 btn btn-info">Credit Card</a>
                     <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=2&is_dd=y" class="align-self-center w-50 mt-3 btn btn-hockney">Direct Debit</a>
@@ -45,13 +45,13 @@
         <div class="mt-4 row mb-4 no-gutters">
             <h3 class="lead" style="font-weight: 500;">Joint Friends</h3>
             <hr />
-            <div class="col-md-4 d-flex border rounded text-center" style="flex-direction: column-reverse; padding: 40px; color: #165181;">
-                <span class="display-6">Joint Friends</span>
+            <div class="col-md-4 d-flex border rounded text-center" style="flex-direction: column-reverse; padding: 40px; color: #165181; margin: -bottom: 16px;">
+                <span class="lead">Joint Friends</span>
                 <figure class="fluid d-flex">
                     <img class="img-fluid" style="max-width: 30%; margin: 0 auto; filter: invert(25%) sepia(77%) saturate(564%) hue-rotate(165deg) brightness(95%) contrast(97%);" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIj4KICA8cGF0aCBkPSJNNC41IDYuMzc1YTQuMTI1IDQuMTI1IDAgMTE4LjI1IDAgNC4xMjUgNC4xMjUgMCAwMS04LjI1IDB6TTE0LjI1IDguNjI1YTMuMzc1IDMuMzc1IDAgMTE2Ljc1IDAgMy4zNzUgMy4zNzUgMCAwMS02Ljc1IDB6TTEuNSAxOS4xMjVhNy4xMjUgNy4xMjUgMCAwMTE0LjI1IDB2LjAwM2wtLjAwMS4xMTlhLjc1Ljc1IDAgMDEtLjM2My42MyAxMy4wNjcgMTMuMDY3IDAgMDEtNi43NjEgMS44NzNjLTIuNDcyIDAtNC43ODYtLjY4NC02Ljc2LTEuODczYS43NS43NSAwIDAxLS4zNjQtLjYzbC0uMDAxLS4xMjJ6TTE3LjI1IDE5LjEyOGwtLjAwMS4xNDRhMi4yNSAyLjI1IDAgMDEtLjIzMy45NiAxMC4wODggMTAuMDg4IDAgMDA1LjA2LTEuMDEuNzUuNzUgMCAwMC40Mi0uNjQzIDQuODc1IDQuODc1IDAgMDAtNi45NTctNC42MTEgOC41ODYgOC41ODYgMCAwMTEuNzEgNS4xNTd2LjAwM3oiIC8+Cjwvc3ZnPgoK" alt="users icons" />
                 </figure>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-8 mt-4">
                 <div class="card h-100 justify-content-center">
                     <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=3" class="align-self-center w-50 btn btn-info">Credit Card</a>
                     <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=3&is_dd=y" class=" align-self-center w-50 mt-3 btn btn-hockney">Direct Debit</a>
