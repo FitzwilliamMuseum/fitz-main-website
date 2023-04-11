42abdf4d4da9e7a51dea99db040b82d27553bf9d
Miro Ostafiński <ostafinskim@gmail.com>
2023-04-11 15:25:06 +0100

    buttons styles

--- resources/views/includes/elements/payment-options.blade.php
+++ resources/views/includes/elements/payment-options.blade.php
@@ -4,16 +4,16 @@
             <h3 class="lead p-heading p-heading--single">Friends individual</h3>
             <hr /> 
             <div class="col-md-4 border d-flex rounded text-center p-container p-container--single">
-                <span class="lead">Friends individual</span>
+                <span>Friends individual</span>
                 <figure class="fluid">
                     <img class="img-fluid p-image p-image--single" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIiA+CiAgPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBkPSJNNy41IDZhNC41IDQuNSAwIDExOSAwIDQuNSA0LjUgMCAwMS05IDB6TTMuNzUxIDIwLjEwNWE4LjI1IDguMjUgMCAwMTE2LjQ5OCAwIC43NS43NSAwIDAxLS40MzcuNjk1QTE4LjY4MyAxOC42ODMgMCAwMTEyIDIyLjVjLTIuNzg2IDAtNS40MzMtLjYwOC03LjgxMi0xLjdhLjc1Ljc1IDAgMDEtLjQzNy0uNjk1eiIgY2xpcC1ydWxlPSJldmVub2RkIiAvPgo8L3N2Zz4KCg==" alt="user icon" />  
                 </figure>
   
             </div>
             <div class="col-md-8 mt-4">
-                <div class="card h-100 justify-content-center">
-                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=2" class="align-self-center w-50 btn btn-info btn-credit">Credit Card ￡55</a>
-                    <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=2&is_dd=y" class="align-self-center w-50 mt-3 btn btn-hockney btn-direct">Direct Debit ￡77</a>
+                <div class="card">
+                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=2" class="btn btn-info btn-credit">Credit Card ￡55</a>
+                    <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=2&is_dd=y" class="mt-3 btn btn-hockney btn-direct">Direct Debit ￡77</a>
                 </div>
             </div>
         </div>
@@ -21,15 +21,15 @@
             <h3 class="lead p-heading p-heading--joint">Joint Friends</h3>
             <hr />
             <div class="col-md-4 border d-flex rounded text-center p-container p-container--joint">
-                <span class="lead">Joint Friends</span>
+                <span>Joint Friends</span>
                 <figure class="fluid">
                     <img class="img-fluid p-image p-image--joint" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIj4KICA8cGF0aCBkPSJNNC41IDYuMzc1YTQuMTI1IDQuMTI1IDAgMTE4LjI1IDAgNC4xMjUgNC4xMjUgMCAwMS04LjI1IDB6TTE0LjI1IDguNjI1YTMuMzc1IDMuMzc1IDAgMTE2Ljc1IDAgMy4zNzUgMy4zNzUgMCAwMS02Ljc1IDB6TTEuNSAxOS4xMjVhNy4xMjUgNy4xMjUgMCAwMTE0LjI1IDB2LjAwM2wtLjAwMS4xMTlhLjc1Ljc1IDAgMDEtLjM2My42MyAxMy4wNjcgMTMuMDY3IDAgMDEtNi43NjEgMS44NzNjLTIuNDcyIDAtNC43ODYtLjY4NC02Ljc2LTEuODczYS43NS43NSAwIDAxLS4zNjQtLjYzbC0uMDAxLS4xMjJ6TTE3LjI1IDE5LjEyOGwtLjAwMS4xNDRhMi4yNSAyLjI1IDAgMDEtLjIzMy45NiAxMC4wODggMTAuMDg4IDAgMDA1LjA2LTEuMDEuNzUuNzUgMCAwMC40Mi0uNjQzIDQuODc1IDQuODc1IDAgMDAtNi45NTctNC42MTEgOC41ODYgOC41ODYgMCAwMTEuNzEgNS4xNTd2LjAwM3oiIC8+Cjwvc3ZnPgoK" alt="users icons" />
                 </figure>
             </div>
             <div class="col-md-8 mt-4">
-                <div class="card h-100 justify-content-center">
-                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=3" class="align-self-center w-50 btn btn-info btn-credit">Credit Card ￡55</a>
-                    <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=3&is_dd=y" class=" align-self-center w-50 mt-3 btn btn-hockney btn-direct">Direct Debit ￡77</a>
+                <div class="card">
+                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=3" class="btn btn-info btn-credit">Credit Card ￡55</a>
+                    <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=3&is_dd=y" class="mt-3 btn btn-hockney btn-direct">Direct Debit ￡77</a>
                 </div>
             </div>
         </div>
