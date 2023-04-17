2b1ec030110d58b0aca910d3159cb97613f94abe
Simon R Jones <simon@studio24.net>
2023-04-17 08:14:11 +0100

    Allow disable Solr on local dev

--- config/solarium.php
+++ config/solarium.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
+    'enabled' => env('SOLR_ENABLED', false),
     'endpoint' => [
         'localhost' => [
-            'host' => env('SOLR_HOST', '131.111.22.71'),
+            'host' => env('SOLR_HOST'),
             'port' => env('SOLR_PORT', '8983'),
             'path' => env('SOLR_PATH', '/'),
-            'core' => env('SOLR_CORE', 'direct')
+            'core' => env('SOLR_CORE')
         ]
     ]
 ];
