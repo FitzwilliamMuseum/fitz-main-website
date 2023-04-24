1adac83f882bd6d58709b00a86a6cf76b1bfacc2
Miro Ostafiński <ostafinskim@gmail.com>
2023-04-24 11:36:52 +0100

    css

--- resources/views/includes/elements/payment-options.blade.php
+++ resources/views/includes/elements/payment-options.blade.php
@@ -1,10 +1,10 @@
 <div class="container mt-4 p-5">
     <div class="card mt-4 col-md-12">
         <div class="row mb-4 no-gutters">
-            <h3 class="lead p-heading p-heading--single">Friends individual</h3>
+            <h3 class="lead p-heading p-heading--single">Individual Friend</h3>
             <hr /> 
-            <div class="col-md-4 border d-flex rounded text-center p-container p-container--single">
-                <span>Friends individual</span>
+            <div class="col-md-4 border d-flex rounded text-center p-container p-container--single" aria-hidden="true">
+                <span>Individual Friend</span>
                 <figure class="fluid">
                     {{--
 
@@ -12,22 +12,25 @@
                         Tool to convert svg into base64: - https://fffuel.co/eeencode/
 
                     --}}
-                    <img class="img-fluid p-image p-image--single" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIiA+CiAgPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBkPSJNNy41IDZhNC41IDQuNSAwIDExOSAwIDQuNSA0LjUgMCAwMS05IDB6TTMuNzUxIDIwLjEwNWE4LjI1IDguMjUgMCAwMTE2LjQ5OCAwIC43NS43NSAwIDAxLS40MzcuNjk1QTE4LjY4MyAxOC42ODMgMCAwMTEyIDIyLjVjLTIuNzg2IDAtNS40MzMtLjYwOC03LjgxMi0xLjdhLjc1Ljc1IDAgMDEtLjQzNy0uNjk1eiIgY2xpcC1ydWxlPSJldmVub2RkIiAvPgo8L3N2Zz4KCg==" alt="user icon" />  
+                    {{-- <img class="img-fluid p-image p-image--single" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIiA+CiAgPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBkPSJNNy41IDZhNC41IDQuNSAwIDExOSAwIDQuNSA0LjUgMCAwMS05IDB6TTMuNzUxIDIwLjEwNWE4LjI1IDguMjUgMCAwMTE2LjQ5OCAwIC43NS43NSAwIDAxLS40MzcuNjk1QTE4LjY4MyAxOC42ODMgMCAwMTEyIDIyLjVjLTIuNzg2IDAtNS40MzMtLjYwOC03LjgxMi0xLjdhLjc1Ljc1IDAgMDEtLjQzNy0uNjk1eiIgY2xpcC1ydWxlPSJldmVub2RkIiAvPgo8L3N2Zz4KCg==" alt="user icon" /> --}}
+                    <svg class="img-fluid p-image p-image--single"  fill="none" viewBox="0 0 24 24" focusable="false" stroke-width="1.5" stroke="currentColor" >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                      </svg>
                 </figure>
   
             </div>
             <div class="col-md-8 mt-4">
                 <div class="card">
-                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=2" class="btn btn-info btn-credit">Credit Card £50/yr</a>
-                    <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=2&is_dd=y" class="mt-3 btn btn-hockney btn-direct">Direct Debit £30/yr</a>
+                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=2" class="btn btn-info btn-credit">Credit Card £30</a>
+                    <a href="https://tickets.museums.cam.ac.uk/components/precart?exec=true&procedureId=54&contrib_type=2&is_dd=Y" class="mt-3 btn btn-hockney btn-direct">Direct Debit £30</a>
                 </div>
             </div>
         </div>
-        <div class="mt-4 row mb-4 no-gutters">
-            <h3 class="lead p-heading p-heading--joint">Joint Friends</h3>
+        <div class="row mb-4 no-gutters">
+            <h3 class="lead p-heading p-heading--joint">Joint Friend</h3>
             <hr />
-            <div class="col-md-4 border d-flex rounded text-center p-container p-container--joint">
-                <span>Joint Friends</span>
+            <div class="col-md-4 border d-flex rounded text-center p-container p-container--joint" aria-hidden="true">
+                <span>Joint Friend</span>
                 <figure class="fluid">
                     {{--
 
@@ -35,13 +38,16 @@
                         Tool to convert svg into base64: - https://fffuel.co/eeencode/
 
                     --}}
-                    <img class="img-fluid p-image p-image--joint" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIj4KICA8cGF0aCBkPSJNNC41IDYuMzc1YTQuMTI1IDQuMTI1IDAgMTE4LjI1IDAgNC4xMjUgNC4xMjUgMCAwMS04LjI1IDB6TTE0LjI1IDguNjI1YTMuMzc1IDMuMzc1IDAgMTE2Ljc1IDAgMy4zNzUgMy4zNzUgMCAwMS02Ljc1IDB6TTEuNSAxOS4xMjVhNy4xMjUgNy4xMjUgMCAwMTE0LjI1IDB2LjAwM2wtLjAwMS4xMTlhLjc1Ljc1IDAgMDEtLjM2My42MyAxMy4wNjcgMTMuMDY3IDAgMDEtNi43NjEgMS44NzNjLTIuNDcyIDAtNC43ODYtLjY4NC02Ljc2LTEuODczYS43NS43NSAwIDAxLS4zNjQtLjYzbC0uMDAxLS4xMjJ6TTE3LjI1IDE5LjEyOGwtLjAwMS4xNDRhMi4yNSAyLjI1IDAgMDEtLjIzMy45NiAxMC4wODggMTAuMDg4IDAgMDA1LjA2LTEuMDEuNzUuNzUgMCAwMC40Mi0uNjQzIDQuODc1IDQuODc1IDAgMDAtNi45NTctNC42MTEgOC41ODYgOC41ODYgMCAwMTEuNzEgNS4xNTd2LjAwM3oiIC8+Cjwvc3ZnPgoK" alt="users icons" />
+                    {{-- <img class="img-fluid p-image p-image--joint" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIj4KICA8cGF0aCBkPSJNNC41IDYuMzc1YTQuMTI1IDQuMTI1IDAgMTE4LjI1IDAgNC4xMjUgNC4xMjUgMCAwMS04LjI1IDB6TTE0LjI1IDguNjI1YTMuMzc1IDMuMzc1IDAgMTE2Ljc1IDAgMy4zNzUgMy4zNzUgMCAwMS02Ljc1IDB6TTEuNSAxOS4xMjVhNy4xMjUgNy4xMjUgMCAwMTE0LjI1IDB2LjAwM2wtLjAwMS4xMTlhLjc1Ljc1IDAgMDEtLjM2My42MyAxMy4wNjcgMTMuMDY3IDAgMDEtNi43NjEgMS44NzNjLTIuNDcyIDAtNC43ODYtLjY4NC02Ljc2LTEuODczYS43NS43NSAwIDAxLS4zNjQtLjYzbC0uMDAxLS4xMjJ6TTE3LjI1IDE5LjEyOGwtLjAwMS4xNDRhMi4yNSAyLjI1IDAgMDEtLjIzMy45NiAxMC4wODggMTAuMDg4IDAgMDA1LjA2LTEuMDEuNzUuNzUgMCAwMC40Mi0uNjQzIDQuODc1IDQuODc1IDAgMDAtNi45NTctNC42MTEgOC41ODYgOC41ODYgMCAwMTEuNzEgNS4xNTd2LjAwM3oiIC8+Cjwvc3ZnPgoK" alt="users icons" /> --}}
+                    <svg class="img-fluid p-image p-image--joint" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" focusable="false">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+                      </svg>
                 </figure>
             </div>
             <div class="col-md-8 mt-4">
                 <div class="card">
-                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=3" class="btn btn-info btn-credit">Credit Card £50/yr</a>
-                    <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=3&is_dd=y" class="mt-3 btn btn-hockney btn-direct">Direct Debit £30/yr</a>
+                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=3" class="btn btn-info btn-credit">Credit Card £48</a>
+                    <a href="https://tickets.museums.cam.ac.uk/components/precart?exec=true&procedureId=54&contrib_type=3&is_dd=Y" class="mt-3 btn btn-hockney btn-direct">Direct Debit £48</a>
                 </div>
             </div>
         </div>
