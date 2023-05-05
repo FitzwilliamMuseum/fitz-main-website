0b50e4a4ff1fa395789c798a8839b0694966ee47
Miro Ostafiński <ostafinskim@gmail.com>
2023-05-05 12:30:29 +0100

    payment-options component and styles

--- resources/views/includes/elements/payment-options.blade.php
+++ resources/views/includes/elements/payment-options.blade.php
@@ -1,49 +1,59 @@
 <div class="container mt-4 p-5">
     <div class="card mt-4 col-md-12">
+
         <div class="row mb-4 no-gutters">
             <h3 class="lead p-heading p-heading--single">Friends individual</h3>
             <hr /> 
             <div class="col-md-4 border d-flex rounded text-center p-container p-container--single">
-                <span>Friends individual</span>
                 <figure class="fluid">
-                    {{--
-
-                        Icons library - https://heroicons.com/
-                        Tool to convert svg into base64: - https://fffuel.co/eeencode/
-
-                    --}}
-                    <img class="img-fluid p-image p-image--single" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIiA+CiAgPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBkPSJNNy41IDZhNC41IDQuNSAwIDExOSAwIDQuNSA0LjUgMCAwMS05IDB6TTMuNzUxIDIwLjEwNWE4LjI1IDguMjUgMCAwMTE2LjQ5OCAwIC43NS43NSAwIDAxLS40MzcuNjk1QTE4LjY4MyAxOC42ODMgMCAwMTEyIDIyLjVjLTIuNzg2IDAtNS40MzMtLjYwOC03LjgxMi0xLjdhLjc1Ljc1IDAgMDEtLjQzNy0uNjk1eiIgY2xpcC1ydWxlPSJldmVub2RkIiAvPgo8L3N2Zz4KCg==" alt="user icon" />  
+                    <img class="img-fluid p-image p-image--single" src="/images/payment-options/friends_individual.jpeg" alt="person in museum" />  
                 </figure>
-  
+         	</div>
+            <div class="col-md-8 d-flex flex-column justify-content-center">
+                <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=2" class="btn btn-info btn-credit">Credit/Debit Card £30/yr</a>
+                <a href="https://tickets.museums.cam.ac.uk/components/precart?exec=true&procedureId=54&contrib_type=2&is_dd=Y" class="mt-3 btn btn-hockney btn-direct">Direct Debit £30/yr</a>
             </div>
-            <div class="col-md-8 mt-4">
-                <div class="card">
-                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=2" class="btn btn-info btn-credit">Credit Card £50/yr</a>
-                    <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=2&is_dd=y" class="mt-3 btn btn-hockney btn-direct">Direct Debit £30/yr</a>
-                </div>
-            </div>
-        </div>
+        </div>	
+
         <div class="mt-4 row mb-4 no-gutters">
             <h3 class="lead p-heading p-heading--joint">Joint Friends</h3>
             <hr />
             <div class="col-md-4 border d-flex rounded text-center p-container p-container--joint">
-                <span>Joint Friends</span>
                 <figure class="fluid">
-                    {{--
-
-                        Icons library - https://heroicons.com/
-                        Tool to convert svg into base64: - https://fffuel.co/eeencode/
-
-                    --}}
-                    <img class="img-fluid p-image p-image--joint" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iY3VycmVudENvbG9yIj4KICA8cGF0aCBkPSJNNC41IDYuMzc1YTQuMTI1IDQuMTI1IDAgMTE4LjI1IDAgNC4xMjUgNC4xMjUgMCAwMS04LjI1IDB6TTE0LjI1IDguNjI1YTMuMzc1IDMuMzc1IDAgMTE2Ljc1IDAgMy4zNzUgMy4zNzUgMCAwMS02Ljc1IDB6TTEuNSAxOS4xMjVhNy4xMjUgNy4xMjUgMCAwMTE0LjI1IDB2LjAwM2wtLjAwMS4xMTlhLjc1Ljc1IDAgMDEtLjM2My42MyAxMy4wNjcgMTMuMDY3IDAgMDEtNi43NjEgMS44NzNjLTIuNDcyIDAtNC43ODYtLjY4NC02Ljc2LTEuODczYS43NS43NSAwIDAxLS4zNjQtLjYzbC0uMDAxLS4xMjJ6TTE3LjI1IDE5LjEyOGwtLjAwMS4xNDRhMi4yNSAyLjI1IDAgMDEtLjIzMy45NiAxMC4wODggMTAuMDg4IDAgMDA1LjA2LTEuMDEuNzUuNzUgMCAwMC40Mi0uNjQzIDQuODc1IDQuODc1IDAgMDAtNi45NTctNC42MTEgOC41ODYgOC41ODYgMCAwMTEuNzEgNS4xNTd2LjAwM3oiIC8+Cjwvc3ZnPgoK" alt="users icons" />
+                    <img class="img-fluid p-image p-image--joint" src="/images/payment-options/friends_joint.jpeg" alt="people talking" />
                 </figure>
             </div>
-            <div class="col-md-8 mt-4">
-                <div class="card">
-                    <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=3" class="btn btn-info btn-credit">Credit Card £50/yr</a>
-                    <a href="https://fitz-tnew-test.tnhs.cloud/components/precart?exec=true&procedureId=54&contrib_type=3&is_dd=y" class="mt-3 btn btn-hockney btn-direct">Direct Debit £30/yr</a>
-                </div>
+            <div class="col-md-8 d-flex flex-column justify-content-center">
+                <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=3" class="btn btn-info btn-credit">Credit/Debit Card £48/yr</a>
+                <a href="https://tickets.museums.cam.ac.uk/components/precart?exec=true&procedureId=54&contrib_type=3&is_dd=y" class="mt-3 btn btn-hockney btn-direct">Direct Debit £48/yr</a>
             </div>
         </div>
+        
+        <div class="row mb-4 no-gutters">
+            <h3 class="lead p-heading p-heading--single">Friends Individual Life</h3>
+            <hr /> 
+            <div class="col-md-4 border d-flex rounded text-center p-container p-container--single">
+                <figure class="fluid">
+                    <img class="img-fluid p-image p-image--single" src="/images/payment-options/friends_individual_life.jpeg" alt="person taking a picture" />  
+                </figure>
+            </div>
+            <div class="col-md-8 d-flex flex-column justify-content-center">
+                <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=4" class="btn btn-info btn-credit">Credit/Debit Card £450/yr</a>
+            </div>
+        </div>
+
+        <div class="row mb-4 no-gutters">
+            <h3 class="lead p-heading p-heading--single">Joint Friends Life</h3>
+            <hr /> 
+            <div class="col-md-4 border d-flex rounded text-center p-container p-container--single">
+                <figure class="fluid">
+                    <img class="img-fluid p-image p-image--single" src="/images/payment-options/friends_joint_life.jpeg" alt="A couple in museum" />  
+                </figure>
+            </div>
+            <div class="col-md-8 d-flex flex-column justify-content-center">
+                <a href="https://tickets.museums.cam.ac.uk/donate/contribute1?ct=5" class="btn btn-info btn-hockney btn-direct">Credit/Debit Card £750/yr</a>
+            </div>
+        </div>
+
     </div>
 </div>
