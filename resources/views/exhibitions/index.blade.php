c6c4a35104dd29be4c5a54b5fe411e2b6c96f406
JamieC24 <jamie@studio24.net>
2023-06-30 10:15:09 +0100

    Sort future events by startDate

--- resources/views/exhibitions/index.blade.php
+++ resources/views/exhibitions/index.blade.php
@@ -57,13 +57,16 @@
     </div>
 @endsection
 @if(!empty($future['data'] ))
+    @php
+        $dataByDate = collect($future['data'])->sortBy('exhibition_start_date');
+    @endphp
     @section('future')
         <div class="container-fluid py-3 bg-grey">
 
             <div class="container">
                 <h3>Our forthcoming exhibitions and displays</h3>
                 <div class="row">
-                    @foreach($future['data'] as $future)
+                    @foreach($dataByDate as $future)
 
                         <x-exhibition-card
                             :altTag="$future['hero_image_alt_text']"
