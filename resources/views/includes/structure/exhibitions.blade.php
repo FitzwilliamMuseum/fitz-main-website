3acf54a25226f99791a0a5f90608e2c7c5b492e8
JamieC24 <jamie@studio24.net>
2023-07-24 17:07:30 +0100

    Amend exhibitions.blade.php to include Many to Many relationship fields

--- resources/views/includes/structure/exhibitions.blade.php
+++ resources/views/includes/structure/exhibitions.blade.php
@@ -3,20 +3,20 @@
         <a href="{{ route('exhibitions')}}">What's on</a>
     </h3>
     <div class="row">
-        {{ json_encode($settings) }}
-        {{-- @foreach($exhibitions['data'] as $current)
+        {{-- {{ json_encode($settings['whats_on']) }} --}}
+        @foreach($settings['whats_on'] as $current)
             <x-exhibition-card
-                :altTag="$current['hero_image_alt_text']"
-                :title="$current['exhibition_title']"
-                :image="$current['hero_image']"
+                :altTag="$current['exhibitions_id']['hero_image_alt_text']"
+                :title="$current['exhibitions_id']['exhibition_title']"
+                :image="$current['exhibitions_id']['hero_image']"
                 :route="'exhibition'"
-                :params="[$current['slug']]"
-                :startDate="$current['exhibition_start_date']"
-                :endDate="$current['exhibition_end_date']"
+                :params="[$current['exhibitions_id']['slug']]"
+                :startDate="$current['exhibitions_id']['exhibition_start_date']"
+                :endDate="$current['exhibitions_id']['exhibition_end_date']"
                 :status="'current'"
-                :ticketed="$current['ticketed']"
-                :tessitura="$current['tessitura_string']"
+                :ticketed="$current['exhibitions_id']['ticketed']"
+                :tessitura="$current['exhibitions_id']['tessitura_string']"
             />
-        @endforeach --}}
+        @endforeach
     </div>
 </div>
