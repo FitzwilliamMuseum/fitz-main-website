66d4861bdf6f60cff81678ee3fe5a282dc15ed20
Gareth Trinkwon <gareth@studio24.net>
2023-07-26 13:27:58 +0100

    details/summary on exhibition FAQs.

--- resources/views/includes/elements/exhibitions/faqs.blade.php
+++ resources/views/includes/elements/exhibitions/faqs.blade.php
@@ -3,13 +3,17 @@
     @if(!empty($faqs))
         <div class="container mt-3">
             <h3>Frequently asked questions</h3>
-
             <div class="col-12 shadow-sm p-3 mx-auto mb-3">
+
+            <details>
+                <summary>Show / Hide</summary>
+                <br>
                 @foreach($faqs as $faq)
                     <x-faq :faq="$faq"></x-faq>
                 @endforeach
+            </details>
             </div>
-            
+
         </div>
     @endif
 
