233c99c5f6c34c365b634c133042d08b9124aedc
Miro Ostafiński <ostafinskim@gmail.com>
2023-07-27 09:25:34 +0100

    parallax background and css fix

--- resources/views/components/home-page-hero.blade.php
+++ resources/views/components/home-page-hero.blade.php
@@ -23,14 +23,12 @@
             </div>
             @if (!empty($hero['ticket_link']))
                 <button class="hero-cta">
-                    <a
-                        href="{{ $hero['ticket_link'] }}">{{ $hero['exhibition_link_text'] }}</a>
+                    <a href="{{ $hero['ticket_link'] }}">{{ $hero['exhibition_link_text'] }}</a>
                 </button>
             @else
                 @if (!empty($hero['exhibition_link']))
                     <button class="hero-cta">
-                        <a
-                            href="{{ route('exhibition', $hero['exhibition_link']['slug']) }}">{{ $hero['exhibition_link_text'] }}</a>
+                        <a href="{{ route('exhibition', $hero['exhibition_link']['slug']) }}">{{ $hero['exhibition_link_text'] }}</a>
                     </button>
                 @endif
             @endif
