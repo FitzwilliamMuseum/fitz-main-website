b4fdaf1e84328096c3126295fa30f9c589855afa
Miro Ostafiński <ostafinskim@gmail.com>
2023-08-10 09:46:52 +0100

    fix: maintenance banner visable

--- resources/views/includes/structure/nav.blade.php
+++ resources/views/includes/structure/nav.blade.php
@@ -52,7 +52,7 @@
 (!empty(SiteHelper::getGlobalAnnouncement()['data'][0]['status']) && SiteHelper::getGlobalAnnouncement()['data'][0]['status'] == 'published')
 )
 <nav class="navbar  navbar-expand-lg navbar-dark bg-black fixed-top container-fluid" style="padding-top: 0; flex-direction: column;">
-    {{-- <div id="global-announcement" class="global-announcement" style="background: #fff; color: black; padding: 10px; text-align: center; width: 100%;">
+    <div id="global-announcement" class="global-announcement" style="background: #fff; color: black; padding: 10px; text-align: center; width: 100%;">
         <style>
             #global-announcement a {
                 color: inherit;
@@ -61,7 +61,7 @@
         </style>
 
         {!! (SiteHelper::getGlobalAnnouncement()['data'][0]['announcement']) !!}
-    </div> --}}
+    </div>
     <div class="container-fluid" style="padding-top: 8px;">
 
 @else
