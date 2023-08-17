69ac723645c7a80dbb0af8e27f3e97bcb064fb61
Sean Dunwoody <sean@studio24.net>
2023-08-17 12:11:16 +0100

    Remove the link to the Twitter bot

--- resources/views/includes/structure/developer.blade.php
+++ resources/views/includes/structure/developer.blade.php
@@ -5,26 +5,6 @@
             <div class="row">
                 <div class="col-md-4 mb-3">
                     <div class="card card-fitz h-100">
-                        <a href="https://twitter.com/fitzArtBot">
-                            <img class="card-img-top"
-                                 src="https://content.fitz.ms/fitz-website/assets/M-P.3_A-1914_4_201702_amt49_mas.jpg?key=exhibition"
-                                 alt="An image showing one of the objects in Shannon and Ricketts Collection"
-                                 loading="lazy"
-                            />
-                        </a>
-                        <div class="card-body h-100">
-                            <div class="contents-label mb-3">
-                                <h2>
-                                    <a href="https://twitter.com/fitzArtBot" class="stretched-link">
-                                        Our Twitter bot
-                                    </a>
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-3">
-                    <div class="card card-fitz h-100">
                         <a href="{{ env('COLLECTION_URL') }}/api">
                             <img class="card-img-top"
                                  src="https://content.fitz.ms/fitz-website/assets/json-peep.png?key=exhibition"
