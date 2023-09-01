8f0bad920335b266af9475c06666949004ad6f97
Miro Ostafiński <ostafinskim@gmail.com>
2023-09-01 14:41:51 +0100

    fix: icon placement

--- resources/views/includes/structure/footer.blade.php
+++ resources/views/includes/structure/footer.blade.php
@@ -59,17 +59,12 @@
                     <div class="mx-auto footer__info px-4 pt-2">
                         <h3 class="visually-hidden">Contact us</h3>
                         <p>
-                            The Fitzwilliam Museum<br/>
+                            Fitzwilliam Museum<br/>
                             Trumpington Street<br/>
                             Cambridge<br/>
                             CB2 1RB<br/>
-                            Switchboard: +44 (0)1223 332 900<br/>
-                            Box office: +44 (0)1223 333 230<br/>
-                            @svg('fas-at',['width'=> 20]) <a
-                                href="mailto:tickets@museums.cam.ac.uk">tickets@museums.cam.ac.uk</a><br/>
-                            What3Words: <span class="w3w"><a
-                                    href="https://map.what3words.com/lofts.puzzle.given"
-                                    aria-label="What3Words location phrase">lofts.puzzle.given</a></span>
+                            +44 (0)1223 332 900<br/>
+                            @svg('fas-at',['width'=> 20]) <a href="mailto:tickets@museums.cam.ac.uk">tickets@museums.cam.ac.uk</a>
                         </p>
                     </div>
                 </div>
@@ -78,41 +73,43 @@
                         <h3 class="visually-hidden">Useful links</h3>
                         <ul class="share">
                             <li>
+                                <a href="{{ route('visit') }}">
+                                    Plan your visit
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('objects') }}">
+                                    Explore our collection
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('landing', ['about-us']) }}">
+                                    About us
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('landing-section', ['commercial-services', 'venue-hire']) }}">
+                                    Venue hire
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('landing-section', ['commercial-services', 'image-library']) }}">
+                                    Image library
+                                </a>
+                            </li>
+
+                            <li>
+                                <a aria-label="Work with us" href="{{ route('vacancies')}}">Work with us</a>
+                            </li>
+                            <li>
                                 <a href="https://www.museums.cam.ac.uk/"
                                    aria-label="The University of Cambridge museums site">University
                                     of Cambridge Museums</a>
                             </li>
                             <li>
-                                <a href="{{ route('landing-section', ['about-us', 'terms-of-use-of-our-website'])}}"
-                                   aria-label="Website terms and conditions">Website Terms of Use</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('landing-section', ['about-us', 'privacy-and-cookies'])}}">Cookies,
-                                    privacy &
-                                    accessibility</a>
-                            </li>
-                            <li>
-                                <a href="#" data-cc="c-settings">Cookie preferences</a>
-                            </li>
-                            <li>
-                                <a aria-label="Our Collections API"
-                                   href="{{env('COLLECTION_URL')}}/api">Our Collections API</a>
-                            </li>
-                            <li>
-                                <a aria-label="Contact the museum"
-                                   href="{{ route('landing-section', ['about-us', 'contact-us'])}}">Contact us</a>
-                            </li>
-                            <li>
-                                <a aria-label="Work for us" href="{{ route('vacancies')}}">Work for us</a>
-                            </li>
-                            <li>
                                 <a href="https://www.registrarysoffice.admin.cam.ac.uk/governance-and-strategy/anti-slavery-and-anti-trafficking"
                                    aria-label="Modern Slavery Act statement of compliance">Modern Slavery Act
-                                    Statement</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('landing-section', ['about-us', 'terms-of-sale'])}}"
-                                   aria-label="Terms of sale for tickets">Terms of Sale</a>
+                                    statement</a>
                             </li>
 
                         </ul>
@@ -122,12 +119,27 @@
                     <div class="mx-auto footer__info px-4 pt-2">
                         <h3 class="visually-hidden">About the University and licenses</h3>
                         <ul class="share">
-                            <li><a href="https://cam.ac.uk/about-the-university/how-the-university-and-colleges-work"
-                                   aria-label="How Collegiate Cambridge works">The University and Colleges</a></li>
-                            <li><a href="https://cam.ac.uk/about-the-university/visiting-the-university"
-                                   aria-label="How to visit the university">Visiting the University</a></li>
-                            <li><a href="https://www.philanthropy.cam.ac.uk/give-now"
-                                   aria-label="How to support the University">Give to Cambridge</a></li>
+                            <li>
+                                <a href="{{ route('landing-section', ['about-us', 'privacy-and-cookies'])}}">Cookies,
+                                    privacy and
+                                    accessibility</a>
+                            </li>
+                            <li>
+                                <a href="#" data-cc="c-settings">Cookie preferences</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('landing-section', ['about-us', 'terms-of-use-of-our-website'])}}"
+                                   aria-label="Website terms and conditions">Website terms of use</a>
+                            </li>
+                            <li>
+                                <a aria-label="Our Collections API"
+                                   href="{{env('COLLECTION_URL')}}/api">Collections API</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('landing-section', ['about-us', 'terms-of-sale'])}}"
+                                   aria-label="Terms of sale for tickets">Terms of sale</a>
+                            </li>
+
                             <li>
                                 <a href="https://creativecommons.org/licenses/by/4.0/"
                                    aria-label="CC-BY license terms">
@@ -165,24 +177,31 @@
             <div class="col-md-6 mx-auto">
                 <h3 class="visually-hidden">Join our conversations</h3>
                 <p class="share text-center">
-                    <a aria-label="Fitzwilliam Museum twitter account" href="https://twitter.com/FitzMuseum_UK">
-                        @svg('fab-twitter',['width'=> 48,'height'=> 48, 'alt' => "Twitter logo", "class" => "my-2 mx-2"])
-
-                    </a>
                     <a aria-label="Fitzwilliam Museum instagram account" href="https://www.instagram.com/fitzmuseum_uk">
-                        @svg('fab-instagram',['width'=> 48,'height'=> 48, 'alt' => "Instagram logo", "class" => "my-2 mx-2"])
-
-                    </a>
-                    <a aria-label="Fitzwilliam Museum linkedin account"
-                       href="https://www.linkedin.com/company/the-fitzwilliam-museum/">
-                        @svg('fab-linkedin',['width'=> 48,'height'=> 48, 'alt' => "Linkedin logo", "class" => "my-2 mx-2"])
-
+                        @svg('fab-instagram',['width'=> 48,'height'=> 48, 'alt' => "Instagram logo", "class" => "my-2
+                        mx-2"])
                     </a>
                     <a aria-label="Fitzwilliam Museum facebook account"
                        href="https://www.facebook.com/fitzwilliammuseum/">
-                        @svg('fab-facebook',['width'=> 48,'height'=> 48, 'alt' => "Facebook logo", "class" => "my-2 mx-2"])
+                        @svg('fab-facebook',['width'=> 48,'height'=> 48, 'alt' => "Facebook logo", "class" => "my-2
+                        mx-2"])
                     </a>
-                    <a aria-label="Fitzwilliam Museum sketchfab account" href="https://www.sketchfab.com/fitzwilliammuseum/">
+                    <a aria-label="Fitzwilliam Museum twitter account" href="https://twitter.com/FitzMuseum_UK">
+                        @svg('fab-twitter',['width'=> 48,'height'=> 48, 'alt' => "Twitter logo", "class" => "my-2
+                        mx-2"])
+                    </a>
+                    <a aria-label="Fitzwilliam Museum linkedin account"
+                        href="https://www.linkedin.com/company/the-fitzwilliam-museum/">
+                         @svg('fab-linkedin',['width'=> 48,'height'=> 48, 'alt' => "Linkedin logo", "class" => "my-2
+                         mx-2"])
+                    </a>
+                    <a aria-label="Watch our YouTube videos"
+                       href="https://www.youtube.com/channel/UCFwhw5uPJWb4wVEU3Y2nScg">
+                        @svg('fab-youtube',['width'=> 48,'height'=> 48, 'alt' => "Youtube logo", "class" => "my-2
+                        mx-2"])
+                    </a>
+                    <a aria-label="Fitzwilliam Museum sketchfab account"
+                       href="https://www.sketchfab.com/fitzwilliammuseum/">
                         <img src="{{ asset( "/images/logos/sketchfab-logo.svg") }}"
                              width="48"
                              height="48"
@@ -192,15 +211,8 @@
                     </a>
                     <a aria-label="Fitzwilliam Museum github account" href="https://www.github.com/fitzwilliammuseum/">
                         @svg('fab-github',['width'=> 48,'height'=> 48, 'alt' => "Github logo", "class" => "my-2 mx-2"])
+                    </a>
 
-                    </a>
-                    <a aria-label="Watch our YouTube videos" href="https://www.youtube.com/channel/UCFwhw5uPJWb4wVEU3Y2nScg">
-                        @svg('fab-youtube',['width'=> 48,'height'=> 48, 'alt' => "Youtube logo", "class" => "my-2 mx-2"])
-
-                    </a>
-                    <a aria-label="Fitzwilliam Museum news feed" href="{{ env('MAIN_URL') }}/news/feed/">
-                        @svg('fas-rss',['width'=> 48,'height'=> 48, 'alt' => "RSS logo", "class" => "my-2 mx-2"])
-                    </a>
                 </p>
             </div>
         </div>
@@ -227,7 +239,10 @@
             id="btn-back-to-top"
             aria-label="Return to the top of the page"
         >
-            <svg height="48" viewBox="0 0 48 48" width="64" xmlns="http://www.w3.org/2000/svg"><path fill="black" id="scrolltop-bg" d="M0 0h48v48h-48z"/><path fill="white" id="scrolltop-arrow" d="M14.83 30.83l9.17-9.17 9.17 9.17 2.83-2.83-12-12-12 12z"/></svg>
+            <svg height="48" viewBox="0 0 48 48" width="64" xmlns="http://www.w3.org/2000/svg">
+                <path fill="black" id="scrolltop-bg" d="M0 0h48v48h-48z"/>
+                <path fill="white" id="scrolltop-arrow" d="M14.83 30.83l9.17-9.17 9.17 9.17 2.83-2.83-12-12-12 12z"/>
+            </svg>
         </button>
     </div>
 </footer>
