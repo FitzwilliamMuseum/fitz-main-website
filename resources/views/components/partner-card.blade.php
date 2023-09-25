79de529ddccf986c856ea561d3e1ce4caae22926
Sean Dunwoody <sean@studio24.net>
2023-09-25 17:36:52 +0100

    Only output links if a link has been set

--- resources/views/components/partner-card.blade.php
+++ resources/views/components/partner-card.blade.php
@@ -1,7 +1,10 @@
 <div class="col-md-4 mb-3">
     <div class="card card-fitz h-100">
-        @isset($image)
+
+        @isset($url)
             <a href="{{ $url }}">
+        @endisset
+            @isset($image)
                 <img class="card-img-top"
                      src="{{ $image['data']['thumbnails'][13]['url']}}"
                      alt="{{ $altTag }}"
@@ -9,22 +12,28 @@
                      width="{{ $image['data']['thumbnails'][13]['width'] }}"
                      height="{{ $image['data']['thumbnails'][13]['height'] }}"
                 />
-            </a>
-        @else
-            <a href="{{ $url }}">
+            @else
                 <img class="card-img-top"
                      src="{{ env('MISSING_IMAGE_URL') }}"
                      alt="A stand in image for {{ $title }}"
                      loading="lazy"
                 />
+            @endisset
+        @isset($url)
             </a>
         @endisset
+
+
         <div class="card-body h-100">
             <div class="contents-label mb-3">
                 <h2>
-                    <a href="{{ $url }}" class="stretched-link">
+                    @isset($url)
+                        <a href="{{ $url }}" class="stretched-link">
+                            {{ $title }}
+                        </a>
+                    @else
                         {{ $title }}
-                    </a>
+                    @endisset
                 </h2>
                 @isset($subtitle)
                     <p class="text-info">{{ $subtitle }}</p>
