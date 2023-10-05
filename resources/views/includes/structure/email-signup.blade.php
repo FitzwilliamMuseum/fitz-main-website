345dc92f4c56f4bb3ce327d49172f90cd717b181
Miro Ostafiński <ostafinskim@gmail.com>
2023-10-05 14:06:36 +0100

    fix: email subscribe link

--- resources/views/includes/structure/email-signup.blade.php
+++ resources/views/includes/structure/email-signup.blade.php
@@ -4,7 +4,7 @@
             <h5>Sign up to our emails</h5>
             <p>Be the first to hear about our news, exhibitions, events and more…</p>
         </div>
-        <a class="subscribre-button" href="https://tickets.museums.cam.ac.uk/account/create">
+        <a class="subscribre-button" href="https://tickets.museums.cam.ac.uk/account/create/brief">
             <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="24" height="25" viewBox="0 0 24 25" fill="none">
                 <g clip-path="url(#clip0_515_199)">
                     <path d="M22 6.5C22 5.4 21.1 4.5 20 4.5H4C2.9 4.5 2 5.4 2 6.5V18.5C2 19.6 2.9 20.5 4 20.5H20C21.1 20.5 22 19.6 22 18.5V6.5ZM20 6.5L12 11.5L4 6.5H20ZM20 18.5H4V8.5L12 13.5L20 8.5V18.5Z" fill="white"/>
