8ed0b3885a97630317f15307f9ce39af803f6843
Miro Ostafiński <ostafinskim@gmail.com>
2023-11-09 13:23:09 +0000

    del: remove duplicate section declaration

--- resources/views/includes/structure/related-pages.blade.php
+++ resources/views/includes/structure/related-pages.blade.php
@@ -1,26 +1,17 @@
+{{--
+Iterates over $records, rendering a card for each $record where 'url' is not 'visiting-guidelines'.
+This excludes a specific card from the "Related to this page" section.
+--}}
 @if(!empty($records))
 @section('associated_pages')
 <div class="container">
     <h3>Related to this page</h3>
     <div class="row">
-        {{--
-        Iterates over $records, rendering a card for each $record where 'url' is not 'visiting-guidelines'.
-        This excludes a specific card from the "Related to this page" section.
-        --}}
-        @if(!empty($records))
-        @section('associated_pages')
-        <div class="container">
-            <h3>Related to this page</h3>
-            <div class="row">
-                @foreach($records as $record)
-                    @if (!isset($record['url']) || $record['url'] !== 'visiting-guidelines')
-                        <x-solr-card :result="$record" />
-                    @endif
-                @endforeach
-            </div>
-        </div>
-        @endsection
-        @endif
+        @foreach($records as $record)
+            @if (!isset($record['url']) || $record['url'] !== 'visiting-guidelines')
+                <x-solr-card :result="$record" />
+            @endif
+        @endforeach
     </div>
 </div>
 @endsection
