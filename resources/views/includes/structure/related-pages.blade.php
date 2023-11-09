135a4ab907b12457700a24925f8ab09cf465399e
Miro Ostafiński <ostafinskim@gmail.com>
2023-11-09 13:11:10 +0000

    fix: filter out non existing page in related content component

--- resources/views/includes/structure/related-pages.blade.php
+++ resources/views/includes/structure/related-pages.blade.php
@@ -1,12 +1,27 @@
 @if(!empty($records))
-    @section('associated_pages')
+@section('associated_pages')
+<div class="container">
+    <h3>Related to this page</h3>
+    <div class="row">
+        {{--
+        Iterates over $records, rendering a card for each $record where 'url' is not 'visiting-guidelines'.
+        This excludes a specific card from the "Related to this page" section.
+        --}}
+        @if(!empty($records))
+        @section('associated_pages')
         <div class="container">
             <h3>Related to this page</h3>
             <div class="row">
                 @foreach($records as $record)
-                    <x-solr-card :result="$record"/>
+                    @if (!isset($record['url']) || $record['url'] !== 'visiting-guidelines')
+                        <x-solr-card :result="$record" />
+                    @endif
                 @endforeach
             </div>
         </div>
-    @endsection
+        @endsection
+        @endif
+    </div>
+</div>
+@endsection
 @endif
