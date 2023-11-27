22381646ccdc94a80359b4c35a8852ba2bd8b903
Miro Ostafiński <ostafinskim@gmail.com>
2023-11-27 12:04:16 +0000

    hotifix: footer icons conflict resolve

--- resources/views/includes/structure/footer.blade.php
+++ resources/views/includes/structure/footer.blade.php
@@ -1,153 +1,47 @@
 <!-- Footer logos -->
 <!--suppress HtmlUnknownAnchorTarget -->
-<div class="bg-white border-cambridge fitz-footer">
-    <div class="col-md-2 col-sm-2 text-center">
-        <a href="https://www.museums.cam.ac.uk">
-            <img class="mx-auto my-2" loading="lazy" width="200" height="62" alt="University of Cambridge Museums logo"
-                src="{{ asset('/images/logos/ucm_logo.svg') }}" />
-        </a>
-    </div>
-    <div class="col-md-2 col-sm-2 text-center">
-        <a href="https://www.cambridge.gov.uk/">
-            <img class="mx-auto my-2" loading="lazy" width="60" height="73.85" alt="Cambridge City Council logo"
-                src="{{ asset('/images/logos/Cambridge_City.svg') }}" />
-        </a>
-    </div>
-    <div class="col-md-2 col-sm-2 text-center">
-        <a href="https://www.artscouncil.org.uk/">
-            <img class="mx-auto my-2" alt="Arts Council England Logo"
-                src="{{ asset('/images/logos/ace_grant_eps_black.svg')}}" loading="lazy" width="200" height="63.3833" />
-        </a>
-    </div>
-    <div class="col-md-2 col-sm-2 text-center">
-        <a href="https://re.ukri.org/">
-            <img class="mx-auto my-2" alt="Research England logo" loading="lazy" width="200" height="63.5"
-                src="{{ asset('images/logos/UKRI_RE-Logo_Horiz-RGB.svg')}}" />
-        </a>
+<div class="bg-white border-cambridge">
+    <div class="fitz-footer">
+        <div class="text-center">
+            <a href="https://www.museums.cam.ac.uk">
+                <img class="mx-auto my-2" loading="lazy" width="200" height="62"
+                    alt="University of Cambridge Museums logo" src="{{ asset('/images/logos/ucm_logo.svg') }}" />
+            </a>
+        </div>
+        <div class="text-center">
+            <a href="https://www.cambridge.gov.uk/">
+                <img class="mx-auto my-2" loading="lazy" width="60" height="73.85" alt="Cambridge City Council logo"
+                    src="{{ asset('/images/logos/Cambridge_City.svg') }}" />
+            </a>
+        </div>
+        <div class="text-center">
+            <a href="https://www.artscouncil.org.uk/">
+                <img class="mx-auto my-2" alt="Arts Council England Logo"
+                    src="{{ asset('/images/logos/ace_grant_eps_black.svg')}}" loading="lazy" width="200"
+                    height="63.3833" />
+            </a>
+        </div>
+        <div class="text-center">
+            <a href="https://re.ukri.org/">
+                <img class="mx-auto my-2" alt="Research England logo" loading="lazy" width="200" height="63.5"
+                    src="{{ asset('images/logos/UKRI_RE-Logo_Horiz-RGB.svg')}}" />
+            </a>
+        </div>
     </div>
 </div>
 
-<footer class="text-black bg-pastel">
-    <div class="container-fluid pt-3">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-4">
-                    <div class="mx-auto footer__info px-4 pt-2">
-                        <h3 class="visually-hidden">Contact us</h3>
-                        <p>
-                            Fitzwilliam Museum<br />
-                            Trumpington Street<br />
-                            Cambridge<br />
-                            CB2 1RB<br />
-                            +44 (0)1223 332 900<br />
-                            @svg('fas-at',['width'=> 20]) <a
-                                href="mailto:tickets@museums.cam.ac.uk">tickets@museums.cam.ac.uk</a>
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="mx-auto footer__info px-4 pt-2">
-                        <h3 class="visually-hidden">Useful links</h3>
-                        <ul class="share">
-                            <li>
-                                <a href="{{ route('visit') }}">
-                                    Plan your visit
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('objects') }}">
-                                    Explore our collection
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('landing', ['about-us']) }}">
-                                    About us
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('landing-section', ['commercial-services', 'venue-hire']) }}">
-                                    Venue hire
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('landing-section', ['commercial-services', 'image-library']) }}">
-                                    Image library
-                                </a>
-                            </li>
-
-                            <li>
-                                <a aria-label="Work with us" href="{{ route('vacancies')}}">Work with us</a>
-                            </li>
-                            <li>
-                                <a href="https://www.museums.cam.ac.uk/"
-                                    aria-label="The University of Cambridge museums site">University
-                                    of Cambridge Museums</a>
-                            </li>
-                            <li>
-                                <a href="https://www.fitzmuseum.cam.ac.uk/about-us/equity-diversity-and-inclusion">Equity,
-                                    diversity and inclusion</a>
-                            </li>
-                            <li>
-                                <a href="https://www.registrarysoffice.admin.cam.ac.uk/governance-and-strategy/anti-slavery-and-anti-trafficking"
-                                    aria-label="Modern Slavery Act statement of compliance">Modern Slavery Act
-                                    statement</a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="mx-auto footer__info px-4 pt-2">
-                        <h3 class="visually-hidden">About the University and licenses</h3>
-                        <ul class="share">
-                            <li>
-                                <a href="{{ route('landing-section', ['about-us', 'privacy-and-cookies'])}}">Cookies,
-                                    privacy and
-                                    accessibility</a>
-                            </li>
-                            <li>
-                                <a href="#" data-cc="c-settings">Cookie preferences</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('landing-section', ['about-us', 'terms-of-use-of-our-website'])}}"
-                                    aria-label="Website terms and conditions">Website terms of use</a>
-                            </li>
-                            <li>
-                                <a aria-label="Our Collections API" href="{{env('COLLECTION_URL')}}/api">Collections
-                                    API</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('landing-section', ['about-us', 'terms-of-sale'])}}"
-                                    aria-label="Terms of sale for tickets">Terms of sale</a>
-                            </li>
-
-                            <li>
-                                <a href="https://creativecommons.org/licenses/by/4.0/" aria-label="CC-BY license terms">
-                                    Content: CC-BY
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://creativecommons.org/share-your-work/public-domain/cc0/"
-                                    aria-label="CC0 license terms">
-                                    Metadata: CC0
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/"
-                                    aria-label="CC-BY-NC-ND license terms">
-                                    Images: CC-BY-NC-ND
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/fitzwilliammuseum/fitz-main-website"
-                                    aria-label="Get the code">
-                                    Code: GPL-V3
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+<footer class="container-fluid pt-3 text-black bg-pastel">
+    <div class="container fitz-footer-links">
+        <div class="pt-2">
+            <h3 class="visually-hidden">Contact us</h3>
+            <p>
+                Fitzwilliam Museum<br />
+                Trumpington Street<br />
+                Cambridge<br />
+                CB2 1RB<br />
+                +44 (0)1223 332 900<br />
+                @svg('fas-at',['width'=> 20]) <a href="mailto:tickets@museums.cam.ac.uk">tickets@museums.cam.ac.uk</a>
+            </p>
         </div>
         <div class="pt-2">
             <h3 class="visually-hidden">Useful links</h3>
