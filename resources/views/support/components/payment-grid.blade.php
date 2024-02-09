935c7ca12973fd1c3ec2834f5d02aabc1e379d5f
JamieC24 <jamie@studio24.net>
2024-02-09 10:49:21 +0000

    Revise staging template

--- resources/views/support/components/payment-grid.blade.php
+++ resources/views/support/components/payment-grid.blade.php
@@ -1,25 +1,30 @@
-<div class="payment-grid">
-    <ul class="grid-container">
-        @if(!empty($component['cta_cards']))
-            @foreach($component['cta_cards'] as $card)
-                <li class="grid-card">
-                    @if(!empty($card['heading']))
-                        <h3>{{ $card['heading'] }}</h3>
-                    @endif
-                    @if(!empty($card['subheading']))
-                        <p>{{ $card['subheading'] }}</p>
-                    @endif
-                    <p><span>{{ !empty($card['highlighted_text']) ? $card['highlighted_text'] : '' }}{{ !empty($card['highlighted_subtext']) ? $card['highlighted_subtext'] : '' }}</span></p>
-                    @if(!empty($card['cta_link']))
-                        <a href="{{ $card['cta_link'] }}">
-                            @if(!empty($card['card_text']))
-                                {{ $card['cta_text'] }}
+
+@if(!empty($component['cta_cards']))
+    <div class="payment">
+        <div class="payment-grid">
+            <ul class="grid-container">
+                @foreach($component['cta_cards'] as $card)
+                    <li class="grid-item">
+                        <div class="grid-card">
+                            @if(!empty($card['heading']))
+                                <h3>{{ $card['heading'] }}</h3>
                             @endif
-                            @svg('fas-chevron-right', ['width' => '16px', 'height' => '16px', 'color' => '#fff'])
-                        </a>
-                    @endif
-                </li>
-            @endforeach
-        @endif
-    </ul>
-</div>
+                            @if(!empty($card['subheading']))
+                                <p>{{ $card['subheading'] }}</p>
+                            @endif
+                            <p><span>{{ !empty($card['highlighted_text']) ? $card['highlighted_text'] : '' }}{{ !empty($card['highlighted_subtext']) ? $card['highlighted_subtext'] : '' }}</span></p>
+                            @if(!empty($card['cta_link']))
+                                <a href="{{ $card['cta_link'] }}">
+                                    @if(!empty($card['card_text']))
+                                        {{ $card['cta_text'] }}
+                                    @endif
+                                    @svg('fas-chevron-right', ['width' => '16px', 'height' => '16px', 'color' => '#fff'])
+                                </a>
+                            @endif
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+@endif
