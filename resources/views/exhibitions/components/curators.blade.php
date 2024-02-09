5739f742e173deda344520b4d7d7089ae4bd8387
JamieC24 <jamie@studio24.net>
2024-02-09 14:58:20 +0000

    Wire up new exhibition template to CMS

--- resources/views/exhibitions/components/curators.blade.php
+++ resources/views/exhibitions/components/curators.blade.php
@@ -9,53 +9,56 @@
         $heading = $exhibition['curators_section_heading'];
     }
 @endphp
-
-<div class="container-fluid related">
-    <div class="container related-container">
-        @if(isset($heading))
-            <h2 class="related-title text-center">{{ $heading }}</h2>
-        @endif
-        <div class="related-grid">
-            @if(isset($associated_curators))
-                {{-- @foreach($associated_curators as $a_curator)
-                    @php
-                        $a_curator = $a_curator['staff_profiles_id']
-                    @endphp
-
-                    @if(!is_null($a_curator))
-
-                        @include('exhibitions.components.curator-card', 
-                        ['curator' => array(
-                            'slug' => $a_curator['slug'],
-                            'profile_image' => $a_curator['profile_image'],
-                            'firstname' => $a_curator['first_name'],
-                            'lastname' => $a_curator['last_name'],
-                            'role' => !is_null($a_curator['job_title']) ? $a_curator['job_title'] : '',
-                            )
-                        ])
-                        
-                    @endif
-                @endforeach --}}
+@if(!empty($exhibition['associated_curators']) || !empty($exhibition['external_curators']))
+    <div class="container-fluid related">
+        <div class="container related-container">
+            @if(isset($heading))
+                <h2 class="related-title text-center">{{ $heading }}</h2>
             @endif
-            @if(isset($external_curators))
-                {{ dd($external_curators) }}
-                {{-- @foreach($external_curators as $e_curator)
-                    @php
-                        $e_curator = $e_curator['associated_people_id']
-                    @endphp
-                    @if(!is_null($a_curator))
-                        @include('exhibitions.components.curator-card', 
-                        ['curator' => array(
-                            'slug' => $e_curator['slug'],
-                            'profile_image' => $e_curator['profile_image'],
-                            'firstname' => $e_curator['firstname'],
-                            'lastname' => $e_curator['lastname'],
-                            'role' => $e_curator['associated_role'],
-                            )
-                        ])
-                    @endif
-                @endforeach --}}
-            @endif
+            <div class="related-grid">
+                @if(isset($associated_curators))
+                    @foreach($associated_curators as $a_curator)
+                        @php
+                            $a_curator = $a_curator['staff_profiles_id']
+                        @endphp
+
+                        @if(!empty($a_curator))
+
+                            @include('exhibitions.components.curator-card', 
+                            ['curator' => array(
+                                'curator_type_slug' => 'about-us/our-staff',
+                                'slug' => !empty($a_curator['slug'])? $a_curator['slug'] : '',
+                                'profile_image' => !empty($a_curator['profile_image'])? $a_curator['profile_image'] : '',
+                                'firstname' => !empty($a_curator['first_name'])? $a_curator['first_name'] : '',
+                                'lastname' => !empty($a_curator['last_name'])? $a_curator['last_name'] : '',
+                                'role' => !empty($a_curator['job_title']) ? $a_curator['job_title'] : '',
+                                )
+                            ])
+                            
+                        @endif
+                    @endforeach
+                @endif
+                @if(isset($external_curators))
+                    {{-- {{ dd($external_curators) }} --}}
+                    @foreach($external_curators as $e_curator)
+                        @php
+                            $e_curator = $e_curator['associated_people_id']
+                        @endphp
+                        @if(!is_null($e_curator))
+                            @include('exhibitions.components.curator-card', 
+                            ['curator' => array(
+                                'curator_type_slug' => 'research/external-curators',
+                                'slug' => !empty($e_curator['slug']) ? $e_curator['slug'] : '',
+                                'profile_image' => !empty($e_curator['profile_image']) ? $e_curator['profile_image'] : '',
+                                'firstname' => !empty($e_curator['firstname']) ? $e_curator['firstname'] : '',
+                                'lastname' => !empty($e_curator['lastname']) ? $e_curator['lastname'] : '',
+                                'role' => !empty($e_curator['associated_role']) ? $e_curator['associated_role'] : '',
+                                )
+                            ])
+                        @endif
+                    @endforeach
+                @endif
+            </div>
         </div>
     </div>
-</div>
+@endif
--- resources/views/exhibitions/templates/details-2024.blade.php
+++ resources/views/exhibitions/templates/details-2024.blade.php
@@ -1,6 +1,6 @@
 <x-exhibition-hero :hero="$hero"></x-exhibition-hero>
 
-<x-exhibition-cta></x-exhibition-cta>
+<x-exhibition-cta :exhibition="$exhibition"></x-exhibition-cta>
 
 @include('support.components.components-repeater', ['page' => $exhibition])
 
