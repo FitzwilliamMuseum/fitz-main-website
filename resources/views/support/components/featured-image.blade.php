51e95c32af96a1e6e46e1d3cb76073fb9b0db57a
JamieC24 <jamie@studio24.net>
2024-02-12 12:41:14 +0000

    Fix image data issue on exhibition pages

--- resources/views/support/components/featured-image.blade.php
+++ resources/views/support/components/featured-image.blade.php
@@ -1,10 +1,18 @@
 @php
     if(!empty($component['image'][0]['image_block_id'])) {
         if(!empty($page['image_blocks'])) {
-            foreach($page['image_blocks'] as $image_block) {
-                if($image_block['asset_id']['id'] == $component['image'][0]['image_block_id']) {
-                    $image_asset = $image_block['asset_id'];
-                }
+            $image_source = $page['image_blocks'];
+        }
+        if(!empty($exhibition['exhibition_files'])) {
+            $image_source = $exhibition['exhibition_files'];
+        }
+
+        foreach($image_source as $image_block) {
+            if($image_block['directus_files_id']) {
+                $image_block['asset_id'] = $image_block['directus_files_id'];
+            }
+            if($image_block['asset_id']['id'] == $component['image'][0]['image_block_id']) {
+                $image_asset = $image_block['asset_id'];
             }
         }
     }
@@ -12,8 +20,9 @@
         $caption = $component['image'][0]['image_caption'];
     }
 @endphp
+    {{-- {{ dd($component['image'][0]) }} --}}
 
-@if (isset($image_asset))
+@if (!empty($image_asset))
     <figure class="container featured_image">
         @if(!empty($image_asset))
             <img src="{{ $image_asset['data']['url'] }}" alt="{{ !empty($image_asset['data']['description']) ? $block_image['data']['description'] : '' }}" load="lazy">
