870d36c104c58b007a5a0edc96542b51a9a512e4
JamieC24 <jamie@studio24.net>
2024-02-12 12:51:04 +0000

    Fix spacing on exhibition cta

--- resources/views/components/exhibition-cta.blade.php
+++ resources/views/components/exhibition-cta.blade.php
@@ -1,6 +1,6 @@
 <div class="exhibition-cta">
     <div class="wrapper">
-        <div class="container col-max-800 mx-auto support-text-component support-cta">
+        <div class="container col-max-800 mx-auto support-text-component support-cta mb-0">
             <h2 class="cta-title exhibition-cta-title">Free</h2>
             <p class="cta-copy exhibition-cta-copy">Our exhibitions are free but we welcome donations</p>
             @if(!empty($exhibition['exhibition_url']))
