8b04aed1d694529520b2e2e2dbb091bfe759533a
JamieC24 <jamie@studio24.net>
2024-02-19 14:35:06 +0000

    Add promo cta support to templates

--- resources/views/components/exhibition-cta.blade.php
+++ resources/views/components/exhibition-cta.blade.php
@@ -14,9 +14,20 @@
         </div>
         <a class="exhibition-cta-link" href="/plan-your-visit">Plan your visit</a>
     </div>
-    @if(!empty($exhibition['exhibition_narrative']))
-        <div class="container col-max-800 mx-auto support-text-component exhibiton-text-compoment">
-            @markdown($exhibition['exhibition_narrative'])
+    @if(!empty($exhibition['exhibition_narrative']) || !empty($exhibition['promo_cta']))
+        <div class="container col-max-800 mx-auto support-text-component exhibition-text-component">
+            @if(!empty($exhibition['exhibition_narrative']))
+                @markdown($exhibition['exhibition_narrative'])
+            @endif
+            @if(!empty($exhibition['promo_cta']))
+                {{-- If the Promo CTA switch is flipped to on / true --}}
+                @if($exhibition['promo_cta'])
+                    <div class="exhibition-cta--promo">
+                        <p>Become a Friend and enjoy unlimited exhibition entry with no need to book.</p>
+                        <a href="/become-a-friend">Find out more</a>
+                    </div>
+                @endif
+            @endif
         </div>
     @endif
 </div>
