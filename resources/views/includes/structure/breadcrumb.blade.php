02e97cd25a8fef76a35a53062f888729476c3580
JamieC24 <jamie@studio24.net>
2024-02-21 15:18:52 +0000

    Add custom breadcrumbs for subpages

--- resources/views/includes/structure/breadcrumb.blade.php
+++ resources/views/includes/structure/breadcrumb.blade.php
@@ -1,3 +1,7 @@
 <div class="{{ isset($class) ? $class : 'col-md-12 shadow-sm p-3 mx-auto mb-3' }}">
-    {{ Breadcrumbs::render() }}
+    @if(!empty($page))
+        @include('components.breadcrumbs')
+    @else
+        {{ Breadcrumbs::render() }}
+    @endif
 </div>
