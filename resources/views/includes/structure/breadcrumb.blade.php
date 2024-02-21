5b06e7f1b60c01f0d7ea7df30a757b3431c993da
JamieC24 <jamie@studio24.net>
2024-02-21 16:18:19 +0000

    Add context for change as comment

--- resources/views/includes/structure/breadcrumb.blade.php
+++ resources/views/includes/structure/breadcrumb.blade.php
@@ -1,4 +1,6 @@
 <div class="{{ isset($class) ? $class : 'col-md-12 shadow-sm p-3 mx-auto mb-3' }}">
+    {{-- When a page is available, use the page title and url as the breadcrumb content --}}
+    {{-- This change was requested to preserve the case of each page title (i.e Support us rather than Support Us) --}}
     @if(!empty($page))
         @include('components.breadcrumbs')
     @else
