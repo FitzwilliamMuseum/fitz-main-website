f4639b6e531665f3e15ead5f10ec41159b2ec458
JamieC24 <jamie@studio24.net>
2024-06-19 16:40:12 +0100

    Add image-gallery component

---
+++ resources/views/support/components/image-gallery.blade.php
@@ -0,0 +1,56 @@
+@if(!empty($component['image_gallery']))
+    <div class="container image-gallery">
+        <div class="row">
+            <div class="carousel slide" data-ride="carousel" data-bs-interval="false" data-pause="hover">
+                <div class="carousel-inner">
+                    @foreach(array_chunk($component['image_gallery'],3,true) as $slides)
+                        <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+                            <div class="row">
+                                @foreach($slides as $image)
+                                    @php
+                                        if(!empty($image[0]['image_id'])) {
+                                            if(!empty($page['image_blocks'])) {
+                                                $image_source = $page['image_blocks'];
+                                            }
+                                            if(!empty($exhibition['exhibition_files'])) {
+                                                $image_source = $exhibition['exhibition_files'];
+                                            }
+
+                                            foreach($image_source as $image_block) {
+                                                if(!empty($image_block['directus_files_id'])) {
+                                                    $image_block['asset_id'] = $image_block['directus_files_id'];
+                                                }
+                                                if($image_block['asset_id']['id'] == $image[0]['image_id']) {
+                                                    $image_asset = $image_block['asset_id'];
+                                            }
+                                            }
+                                        }
+                                    @endphp
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card gallery-card">
+                                            @if(!empty($image_asset))
+                                                <img src="{{ $image_asset['data']['url'] }}" alt="{{ !empty($image_asset['data']['description']) ? $block_image['data']['description'] : '' }}" load="lazy">
+                                            @else
+                                                <img
+                                                    src="{{ env('MISSING_IMAGE_URL') }}"
+                                                    load="lazy" alt="">
+                                            @endif
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#contexts" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#contexts" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+@endif
