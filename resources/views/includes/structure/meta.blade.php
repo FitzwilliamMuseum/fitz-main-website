3e4344abcef1e62c3cbc9dc2b7a00f9dc9c0ca9b
Miro Ostafiński <ostafinskim@gmail.com>
2025-01-16 14:45:04 +0000

    whitespace remove

--- resources/views/includes/structure/meta.blade.php
+++ resources/views/includes/structure/meta.blade.php
@@ -10,12 +10,12 @@
 @endif
 <!-- Canonical link -->
 <link rel="canonical" href="{{ URL::current() }}"/>
-<link rel="shortcut icon" href="{{ secure_url('/') }}/favicon.ico">
+<link rel="shortcut icon" href="{{secure_url('/')}}/favicon.ico">
 
 <!-- Open graph -->
 <meta property="og:locale" content="{{ app()->getLocale() }}"/>
 <meta property="og:type" content="website"/>
-<meta property="og:title" content="@yield('title')" />
+<meta property="og:title" content="@yield('title')"/>
 <meta property="og:description" content="@yield('description')"/>
 <meta property="og:url" content="{{ URL::current() }}"/>
 <meta property="og:site_name" content="The Fitzwilliam Museum"/>
@@ -30,7 +30,7 @@
 <!-- Twitter card -->
 <meta name="twitter:card" content="summary"/>
 <meta name="twitter:description" content="@yield('description')"/>
-<meta name="twitter:title" content="@yield('title')" />
+<meta name="twitter:title" content="@yield('title')"/>
 <meta name="twitter:site" content="@yield('twitter_id', '@fitzmuseum_uk')"/>
 <meta name="twitter:creator" content="@yield('twitter_id', '@fitzmuseum_uk')"/>
 
