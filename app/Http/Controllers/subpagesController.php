8944066bb5a056650adaa63fcea5b51c50dcedad
JamieC24 <jamie@studio24.net>
2025-07-23 12:17:56 +0100

    Fix subpages controller

--- app/Http/Controllers/subpagesController.php
+++ app/Http/Controllers/subpagesController.php
@@ -13,16 +13,16 @@ class subpagesController extends Controller
      * @param string $slug
      * @return View|Response
      */
-    public function index(string $parent_page, string $slug): View|Response
+    public function index(string $slug): View|Response
     {
-        $parent = LandingPageTemplate::getLanding($parent_page);
+        $parent_page = LandingPageTemplate::getLanding();
         $page = LandingPageTemplate::getSubpage($slug);
         if (empty($page['data'])) {
             return response()->view('errors.404', [], 404);
         } else {
             return view('support.subpage', [
                 'page' => Collect($page['data'])->first(),
-                'parent_page' => Collect($parent['data'])->first()
+                'parent_page' => Collect($parent_page['data'])->first()
             ]);
         }
     }
