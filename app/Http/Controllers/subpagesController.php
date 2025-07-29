16936f5969d2066ae82c4a63607f64610f668309
JamieC24 <jamie@studio24.net>
2025-07-29 13:10:54 +0100

    Add contingency for plan-your-visit subpages that live in the stubs collection

--- app/Http/Controllers/subpagesController.php
+++ app/Http/Controllers/subpagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\LandingPageTemplate;
+use App\Models\Stubs;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Response;
@@ -17,8 +18,23 @@ class subpagesController extends Controller
     {
         $parent_page = LandingPageTemplate::getLanding();
         $page = LandingPageTemplate::getSubpage($slug);
+        
         if (empty($page['data'])) {
-            return response()->view('errors.404', [], 404);
+            // Contingency for old visit us pages - not sure if this is the best approach, but it works for now
+            if(str_contains($_SERVER['REQUEST_URI'], 'plan-your-visit')) {
+                $page = Stubs::getPage('plan-your-visit', $slug);
+
+                if(!empty($page['data'])) {
+                    return view('pages.index', [
+                            'page' => Collect($page['data'])->first()
+                        ]
+                    );
+                } else {
+                    return response()->view('errors.404', [], 404);
+                }
+            } else {
+                return response()->view('errors.404', [], 404);
+            }
         } else {
             return view('support.subpage', [
                 'page' => Collect($page['data'])->first(),
