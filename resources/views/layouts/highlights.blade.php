13d9faf002c16497c8fa556e6fc89d33ec425ba6
Chris Butterworth <chris@studio24.net>
2025-08-14 14:36:39 +0100

    Changing script to include

--- resources/views/layouts/highlights.blade.php
+++ resources/views/layouts/highlights.blade.php
@@ -14,25 +14,7 @@
     @yield('jsonld')
 
     <x-feed-links></x-feed-links>
-    <script>
-        // Include the following lines to define the gtag() function when
-        // calling this code prior to your gtag.js or Tag Manager snippet
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        // Call the default command before gtag.js or Tag Manager runs to
-        // adjust how the tags operate when they run. Modify the defaults
-        // per your business requirements and prior consent granted/denied, e.g.:
-        gtag('consent', 'default', {
-            'ad_storage': 'denied',
-            'ad_user_data': 'denied',
-            'ad_personalization': 'denied',
-            'analytics_storage': 'denied'
-        });
-
-    </script>
+    @include('includes.scripts.gtagconsent')
     @include('googletagmanager::head')
 
 </head>
