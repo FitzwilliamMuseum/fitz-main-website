185995916ebff1f81d4b7985c451bc79d8e93a39
Miro Ostafiński <ostafinskim@gmail.com>
2025-08-14 12:55:46 +0100

    fix: visit-us grid additional spacing remove

--- resources/views/visit/index.blade.php
+++ resources/views/visit/index.blade.php
@@ -17,7 +17,7 @@
 @extends('layouts.visitus')
 
 @section('hero_image', 'https://fitz-cms-images.s3.eu-west-2.amazonaws.com/visit-final-.png')
-@section('title', $title);
+@section('title', $title)
 @section('description', 'Visiting the Fitzwilliam Museum? What do you need to know?')
 @section('keyword', 'cambridge,museums,visit')
 @php
