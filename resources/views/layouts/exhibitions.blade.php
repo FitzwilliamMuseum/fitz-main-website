4bd842fb907cf8c9cdb9dcf8468a72c0e7f0b1bf
Chris Butterworth <chris@studio24.net>
2025-08-14 14:26:19 +0100

    Adding gtag consent default to all layout templates

--- resources/views/layouts/exhibitions.blade.php
+++ resources/views/layouts/exhibitions.blade.php
@@ -14,7 +14,27 @@
     @include('includes.css.css')
 
     @include('includes.structure.manifest')
+    <script>
+        // Include the following lines to define the gtag() function when
+        // calling this code prior to your gtag.js or Tag Manager snippet
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        // Call the default command before gtag.js or Tag Manager runs to
+        // adjust how the tags operate when they run. Modify the defaults
+        // per your business requirements and prior consent granted/denied, e.g.:
+        gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'ad_user_data': 'denied',
+            'ad_personalization': 'denied',
+            'analytics_storage': 'denied'
+        });
+
+    </script>
     @include('googletagmanager::head')
+
 
 </head>
 
--- resources/views/layouts/ttn-iiif.blade.php
+++ resources/views/layouts/ttn-iiif.blade.php
@@ -13,6 +13,25 @@
             height: 100vh;
         }
     </style>
+    <script>
+        // Include the following lines to define the gtag() function when
+        // calling this code prior to your gtag.js or Tag Manager snippet
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        // Call the default command before gtag.js or Tag Manager runs to
+        // adjust how the tags operate when they run. Modify the defaults
+        // per your business requirements and prior consent granted/denied, e.g.:
+        gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'ad_user_data': 'denied',
+            'ad_personalization': 'denied',
+            'analytics_storage': 'denied'
+        });
+
+    </script>
 </head>
 <body class="doc-body c_darkmode">
 @include('googletagmanager::body')
