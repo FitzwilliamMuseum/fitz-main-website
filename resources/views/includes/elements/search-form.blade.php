b797163b325cadd5560db06eb22ef3f9b4a9b51b
Daniel Pett <danielpett@gmail.com>
2025-08-26 22:48:51 +0100

    Fix forms for spatie html for laravel 12

--- resources/views/includes/elements/search-form.blade.php
+++ resources/views/includes/elements/search-form.blade.php
@@ -1,26 +1,29 @@
 <div class="col-12 col-max-800 shadow-sm p-3 mx-auto mb-3">
-    {{ Form::open(['url' => url('search/results'),'method' => 'GET']) }}
+    {{ html()->form('get', route('search.results'))->class('your-form-class')->open() }}
     <div class="row center-block">
         <div class="col-lg-6 center-block">
             <div class="input-group">
-                <input type="text" id="query" name="query" class="form-control me-2" placeholder="Search our site"
-                    required value="{{ old('query') }}">
+                {{ html()->text('query', old('query'))
+                    ->class('form-control me-2')
+                    ->id('query')
+                    ->placeholder('Search our site')
+                    ->required() }}
                 <span class="input-group-btn">
-                    <button class="btn btn-dark" type="submit">Search...</button>
+                    {{ html()->submit('Search...')->class('btn btn-dark') }}
                 </span>
             </div>
         </div>
     </div>
     @if(count($errors))
-    <div class="form-group">
-        <div class="alert alert-danger">
-            <ul>
-                @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+        <div class="form-group">
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-    </div>
     @endif
-    {!! Form::close() !!}
+    {{ html()->form()->close() }}
 </div>
