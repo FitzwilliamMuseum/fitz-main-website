81beb012810695e44bda988718d0629c2897b03c
JamieC24 <jamie@studio24.net>
2025-10-10 09:46:40 +0100

    Adjust 404 / 500 pages to remove the image of grumpy cat :(

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -6,16 +6,7 @@
 
 <div class="col-12 col-max-800 shadow-sm p-3 mx-auto mb-3 mt-3">
     <div class="row">
-        <div class="col-md-6">
-            <figure class="figure">
-                <img alt="An image of a very grumpy cat" class="img-fluid" width="416" height="416"
-                    src="{{ env('CONTENT_STORE') }}searle_cat.jpg?key=exhibition" />
-                <figcaption class="figure-caption">
-                    One of Ronald Searle's cats, given to the Fitzwilliam Museum in 2014 by his family.
-                </figcaption>
-            </figure>
-        </div>
-        <div class="col-md-6">
+        <div class="container">
             <p>
                 We are very sorry, but our website has encountered a problem.
             </p>
